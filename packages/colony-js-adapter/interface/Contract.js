32619f1a52b6f169f37d57a9dfae0f615b9f123d
James Lefrère <james@candis.io>
2018-02-19 16:47:28 +0100

    Loosely type/move typings

--- packages/colony-js-adapter/interface/Contract.js
+++ packages/colony-js-adapter/interface/Contract.js
@@ -1,30 +1,13 @@
 /* @flow */
 
-import type BigNumber from 'bn.js';
-
 import type { Event } from './Event';
 import type { Provider } from './Provider';
-import type { Transaction } from './Transaction';
-
-export type TransactionOptions = {
-  gasLimit?: number,
-  gasPrice?: number,
-  nonce?: number,
-  value?: BigNumber,
-};
-
-export type ContractFn<T> = (...T) => Promise<Transaction>;
-export type ContractEstimateFn = (...*) => Promise<BigNumber>;
 
 export interface Contract {
   address: string;
   constructor(address: string, abi: {}, provider: Provider): Contract;
-  estimate: {
-    [string]: Function,
-  };
-  functions: {
-    [string]: Function,
-  };
+  estimate: *;
+  functions: *;
   events: {
     // e.g. onMyEventName
     [string]: (Function) => void,
@@ -33,7 +16,7 @@ export interface Contract {
   addListener(
     eventName: string,
     transactionHash: string,
-    callback: (event: Event) => *,
+    callback: (event: Event) => void,
   ): void;
   removeListener(eventName: string, transactionHash: string): void;
 }
