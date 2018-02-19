bba2f26f67f1983ce05ccd37a0d216c542ffedc6
James Lefrère <james@candis.io>
2018-02-19 16:48:10 +0100

    Refactor Adapter interface

--- packages/colony-js-adapter/index.js
+++ packages/colony-js-adapter/index.js
@@ -3,12 +3,10 @@
 export type { Adapter as IAdapter } from './interface/Adapter';
 export type { Block } from './interface/Block';
 export type { Event } from './interface/Event';
-export type {
-  Contract as IContract,
-  ContractEstimateFn,
-  ContractFn,
-  TransactionOptions,
-} from './interface/Contract';
+export type { EventHandlers } from './interface/EventHandlers';
+export type { Contract as IContract } from './interface/Contract';
 export type { Provider as IProvider } from './interface/Provider';
 export type { MinedTransaction, Transaction } from './interface/Transaction';
 export type { TransactionReceipt } from './interface/TransactionReceipt';
+export type { TransactionOptions } from './interface/TransactionOptions';
+export type { Wallet as IWallet } from './interface/Wallet';
--- packages/colony-js-adapter/interface/Adapter.js
+++ packages/colony-js-adapter/interface/Adapter.js
@@ -1,23 +1,29 @@
 /* @flow */
 
-import type { Query, IContractLoader } from '@colony/colony-js-contract-loader';
+import type {
+  Options as LoaderOptions,
+  IContractLoader,
+} from '@colony/colony-js-contract-loader';
 
 import type { Contract } from './Contract';
-import type { Event } from './Event';
+import type { EventHandlers } from './EventHandlers';
 import type { Provider } from './Provider';
 import type { MinedTransaction } from './Transaction';
 import type { TransactionReceipt } from './TransactionReceipt';
 
-export interface Adapter<LoaderOptions> {
-  _loader: IContractLoader<LoaderOptions>;
+export interface Adapter<ContractInterface: Contract> {
+  _loader: IContractLoader;
   _provider: Provider;
-  getContract(query: Query, options?: LoaderOptions): Promise<Contract>;
-  getEventPromises({
-    contract: Contract,
-    events: Array<string>,
+  getContract(
+    contractName: string,
+    loaderOptions?: LoaderOptions,
+  ): Promise<ContractInterface>;
+  getEventData({
+    contract: ContractInterface,
+    events: EventHandlers,
     timeoutMs: number,
     transactionHash: string,
-  }): Array<Promise<Event>>;
+  }): Promise<*>;
   getTransactionReceipt(transactionHash: string): Promise<TransactionReceipt>;
   waitForTransaction(
     transactionHash: string,
