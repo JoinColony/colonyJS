f59009fcd6a31c98b9aa636f3097fc59f2bf9264
James Lefrère <james@candis.io>
2018-02-19 16:46:14 +0100

    Add Encryption options

---
+++ packages/colony-js-adapter/interface/Encryption.js
@@ -0,0 +1,14 @@
+export type Encryption = {
+  client?: string,
+  entropy?: string,
+  iv?: string,
+  mnemonic?: string,
+  path?: string,
+  scrypt?: {
+    N?: number, // Cost
+    p?: number, // Parallelisation
+    r?: number, // Block size
+  },
+  salt?: string,
+  uuid?: string,
+};
