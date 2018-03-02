cbb9bf138f66da7c63f4e48188fff5febecb8137
James Lefrère <james@candis.io>
2018-03-02 17:41:53 +0100

    Caller overhaul
    * Use ParamPairs for arguments
    * Map over returnValues for the result
    * Also add static Caller creation

--- packages/colony-js-contract-client/src/modules/Caller.js
+++ packages/colony-js-contract-client/src/modules/Caller.js
@@ -1,42 +1,73 @@
 /* @flow */
 
+import BigNumber from 'bn.js';
 import utils from '@colony/colony-js-utils';
+import type { CallFn } from '@colony/colony-js-adapter';
 
 import { DEFAULT_TIMEOUT } from '../constants';
 import Validator from './Validator';
 import ContractClient from './ContractClient';
 
+import type { CallerDef, ParamTypePairs, ParamTypes } from '../types';
+
 export default class Caller<
-  Params,
+  Params: { [name: string]: * },
   ReturnValue,
   // eslint-disable-next-line
   IContractClient: ContractClient<*>
 > extends Validator<Params> {
-  +_call: *;
+  +_call: CallFn<*, *>;
   client: IContractClient;
-  constructor(client: IContractClient) {
-    super();
-    this.client = client;
+  static returnValues: ParamTypePairs = [];
+  static create(
+    client: IContractClient,
+    { params = [], returnValues, call }: CallerDef,
+  ): Caller<Params, ReturnValue, IContractClient> {
+    class _Caller extends Caller<Params, ReturnValue, IContractClient> {
+      static params = params;
+      static returnValues = returnValues;
+    }
+    return new _Caller(client, call);
+  }
+  static parseReturnValue(value: *, type: ParamTypes) {
+    switch (type) {
+      case 'number':
+        return BigNumber.isBN(value) ? value.toNumber() : value;
+      default:
+        return value;
+    }
   }
   // eslint-disable-next-line no-unused-vars
   static parseReturn(values: *, params: Params): ReturnValue {
-    throw new TypeError(
-      'Caller.parseReturn should be extended in a derived class',
-    );
+    return this.returnValues.length
+      ? // $FlowFixMe Object literal incompatible with ReturnValue; perhaps try $ObjMap?
+        Object.assign(
+          {},
+          ...this.returnValues.map(([name, type], index) => ({
+            [name]: this.parseReturnValue(values[index], type),
+          })),
+        )
+      : values;
+  }
+  constructor(client: IContractClient, call?: CallFn<*, *>) {
+    super();
+    this.client = client;
+    if (typeof call === 'function') this._call = call;
   }
   async call(
     params: Params,
     { timeoutMs = DEFAULT_TIMEOUT }: { timeoutMs: number } = {},
   ): Promise<ReturnValue> {
-    this.validate(params);
-    const args =
-      typeof this.constructor.parseParams === 'function'
-        ? this.constructor.parseParams(params)
-        : [];
+    if (typeof this._call !== 'function')
+      throw new TypeError('Expected a call function for Caller');
+
+    const args = this.constructor.getArgs(params);
+
     const values = await utils.raceAgainstTimeout(
       this._call(...args),
       timeoutMs,
     );
+
     return this.constructor.parseReturn(values, params);
   }
 }
