0b0c5a92779e3558ea82b9154a3d97169dfc1c34
Raul Glogovetan <raul@glogovetan.com>
2018-03-28 12:45:12 +0300

    Add: submodules webpack build mode config

--- packages/colony-js-utils/webpack.config.js
+++ packages/colony-js-utils/webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
 
+let mode = 'development';
+
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+
 module.exports = {
   entry: './src/index.js',
+  mode,
   output: {
     filename: 'colony-js-utils.js',
     path: path.resolve(__dirname, 'dist'),
