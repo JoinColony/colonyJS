7bc212a21a55ecd0863a72675807d1d7c186d8f8
James Lefrère <james@candis.io>
2018-04-04 12:38:59 +0100

    Update getTask contract iface

--- packages/colony-js-client/src/interface/ColonyContract.js
+++ packages/colony-js-client/src/interface/ColonyContract.js
@@ -170,10 +170,21 @@ export interface ColonyContract extends IContract {
     // uint256 _potId, address _token => uint256
     getPotBalance: CallFn<[number, string], number>,
 
-    // uint256 => bytes32, bytes32, bool, bool, uint256, uint256, uint256, uint256
+    // uint256 => bytes32, bytes32, bool, bool, uint256, uint256, uint256, uint256, uint256, uint256[]
     getTask: CallFn<
       [number],
-      [string, string, boolean, boolean, number, number, number, number],
+      [
+        string,
+        string,
+        boolean,
+        boolean,
+        number,
+        number,
+        number,
+        number,
+        number,
+        [number], // Currently just one item for skills
+      ],
     >,
 
     // => uint256
