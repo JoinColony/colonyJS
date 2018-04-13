9eb5412b759794bd6074bca2d4bd69caec6e9a45
James Lefrère <james@colony.io>
2018-04-13 18:43:13 +0200

    Export Loaders
    * Export the base class as a default
    * Also export the specific loader classes

---
+++ packages/colony-js-contract-loader-http/src/index.js
@@ -0,0 +1,8 @@
+/* @flow */
+
+import ContractHttpLoader from './loaders/ContractHttpLoader';
+import EtherscanLoader from './loaders/EtherscanLoader';
+import TrufflepigLoader from './loaders/TrufflepigLoader';
+
+export default ContractHttpLoader;
+export { ContractHttpLoader, EtherscanLoader, TrufflepigLoader };
