c2520686b19bc052a6274f5cb528fb10d7ccb651
James Lefrère <james@colony.io>
2018-04-13 18:42:09 +0200

    Add `EtherscanLoader`

---
+++ packages/colony-js-contract-loader-http/loaders/EtherscanLoader.js
@@ -0,0 +1,34 @@
+/* @flow */
+
+import type { Query } from '@colony/colony-js-contract-loader';
+
+import ContractHttpLoader from './ContractHttpLoader';
+
+const DEFAULT_ENDPOINT =
+  'https://api.etherscan.io/api?module=contract&action=getabi&address=%%ADDRESS%%'; // eslint-disable-line max-len
+
+export default class EtherscanLoader extends ContractHttpLoader {
+  constructor({ endpoint = DEFAULT_ENDPOINT }: { endpoint: string } = {}) {
+    super({ endpoint });
+  }
+  static parse(response: any, query: Query) {
+    if (
+      !(
+        typeof response === 'object' &&
+        Object.hasOwnProperty.call(response, 'status')
+      )
+    ) {
+      throw new Error('Malformed response from Etherscan');
+    }
+
+    const { result: abi, status } = response;
+
+    if (status !== '1')
+      throw new Error(`Erroneous response from Etherscan (status: ${status})`);
+
+    return {
+      abi,
+      address: query.contractAddress,
+    };
+  }
+}
