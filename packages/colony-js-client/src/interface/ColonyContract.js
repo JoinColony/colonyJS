28c813a96f7147baa194ac784b57a3e9797376c4
James Lefrère <james@colony.io>
2018-04-23 16:41:16 +0200

    Contract iface improvements
    * Made the contract interfaces less prescriptive (and less reflective of `ethers`)
    * Now functions are defined at the root level of the interface

--- packages/colony-js-client/src/interface/ColonyContract.js
+++ packages/colony-js-client/src/interface/ColonyContract.js
@@ -1,12 +1,6 @@
 /* @flow */
 
-import type {
-  IContract,
-  CallFn,
-  InterfaceFn,
-  EstimateFn,
-  SendFn,
-} from '@colony/colony-js-adapter';
+import type { IContract, CallFn, SendFn } from '@colony/colony-js-adapter';
 
 type sendFns = {
   // address _token
@@ -54,15 +48,6 @@ type sendFns = {
   // uint256 _id, uint256 _dueDate
   setTaskDueDate: [number, number],
 
-  // uint256 _id, bytes32 _deliverableHash
-  submitTaskDeliverable: [number, string],
-
-  // uint256 _id
-  finalizeTask: [number],
-
-  // uint256 _id
-  cancelTask: [number],
-
   // uint256 _id, address _token, uint256 _amount
   setTaskManagerPayout: [number, string, number],
 
@@ -71,6 +56,15 @@ type sendFns = {
 
   // uint256 _id, address _token, uint256 _amount
   setTaskWorkerPayout: [number, string, number],
+
+  // uint256 _id, bytes32 _deliverableHash
+  submitTaskDeliverable: [number, string],
+
+  // uint256 _id
+  finalizeTask: [number],
+
+  // uint256 _id
+  cancelTask: [number],
 
   // uint256 _id, uint256 _role, address _token
   claimPayout: [number, number, string],
@@ -92,165 +86,84 @@ type sendFns = {
 };
 
 export interface ColonyContract extends IContract {
-  interface: {
-    functions: {
-      // XXX until https://github.com/facebook/flow/issues/3237 is fixed, we can't type these more
-      addDomain: InterfaceFn<*>,
-      addGlobalSkill: InterfaceFn<*>,
-      approveTaskChange: InterfaceFn<*>,
-      assignWorkRating: InterfaceFn<*>,
-      cancelTask: InterfaceFn<*>,
-      claimColonyFunds: InterfaceFn<*>,
-      claimPayout: InterfaceFn<*>,
-      finalizeTask: InterfaceFn<*>,
-      makeTask: InterfaceFn<*>,
-      mintTokens: InterfaceFn<*>,
-      mintTokensForColonyNetwork: InterfaceFn<*>,
-      moveFundsBetweenPots: InterfaceFn<*>,
-      proposeTaskChange: InterfaceFn<*>,
-      revealTaskWorkRating: InterfaceFn<*>,
-      setTaskBrief: InterfaceFn<*>,
-      setTaskDomain: InterfaceFn<*>,
-      setTaskDueDate: InterfaceFn<*>,
-      setTaskEvaluatorPayout: InterfaceFn<*>,
-      setTaskManagerPayout: InterfaceFn<*>,
-      setTaskRoleUser: InterfaceFn<*>,
-      setTaskSkill: InterfaceFn<*>,
-      setTaskWorkerPayout: InterfaceFn<*>,
-      submitTaskDeliverable: InterfaceFn<*>,
-      submitTaskWorkRating: InterfaceFn<*>,
-    },
-  };
-  estimate: {
-    addDomain: EstimateFn<$PropertyType<sendFns, 'addDomain'>>,
-    approveTaskChange: EstimateFn<$PropertyType<sendFns, 'approveTaskChange'>>,
-    assignWorkRating: EstimateFn<$PropertyType<sendFns, 'assignWorkRating'>>,
-    cancelTask: EstimateFn<$PropertyType<sendFns, 'cancelTask'>>,
-    claimColonyFunds: EstimateFn<$PropertyType<sendFns, 'claimColonyFunds'>>,
-    claimPayout: EstimateFn<$PropertyType<sendFns, 'claimPayout'>>,
-    finalizeTask: EstimateFn<$PropertyType<sendFns, 'finalizeTask'>>,
-    makeTask: EstimateFn<$PropertyType<sendFns, 'makeTask'>>,
-    mintTokens: EstimateFn<$PropertyType<sendFns, 'mintTokens'>>,
-    mintTokensForColonyNetwork: EstimateFn<
-      $PropertyType<sendFns, 'mintTokensForColonyNetwork'>,
-    >,
-    moveFundsBetweenPots: EstimateFn<
-      $PropertyType<sendFns, 'moveFundsBetweenPots'>,
-    >,
-    revealTaskWorkRating: EstimateFn<
-      $PropertyType<sendFns, 'revealTaskWorkRating'>,
-    >,
-    setTaskBrief: EstimateFn<$PropertyType<sendFns, 'setTaskBrief'>>,
-    setTaskDomain: EstimateFn<$PropertyType<sendFns, 'setTaskDomain'>>,
-    setTaskDueDate: EstimateFn<$PropertyType<sendFns, 'setTaskDueDate'>>,
-    setTaskEvaluatorPayout: EstimateFn<
-      $PropertyType<sendFns, 'setTaskEvaluatorPayout'>,
-    >,
-    setTaskManagerPayout: EstimateFn<
-      $PropertyType<sendFns, 'setTaskManagerPayout'>,
-    >,
-    setTaskRoleUser: EstimateFn<$PropertyType<sendFns, 'setTaskRoleUser'>>,
-    setTaskSkill: EstimateFn<$PropertyType<sendFns, 'setTaskSkill'>>,
-    setTaskWorkerPayout: EstimateFn<
-      $PropertyType<sendFns, 'setTaskWorkerPayout'>,
-    >,
-    submitTaskDeliverable: EstimateFn<
-      $PropertyType<sendFns, 'submitTaskDeliverable'>,
-    >,
-    submitTaskWorkRating: EstimateFn<
-      $PropertyType<sendFns, 'submitTaskWorkRating'>,
-    >,
-    addGlobalSkill: EstimateFn<$PropertyType<sendFns, 'addGlobalSkill'>>,
-    proposeTaskChange: EstimateFn<$PropertyType<sendFns, 'proposeTaskChange'>>,
-  };
-  functions: {
-    // address => uint256
-    getNonRewardPotsTotal: CallFn<[string], number>,
+  // address => uint256
+  getNonRewardPotsTotal: CallFn<[string], number>;
 
-    // uint256 _potId, address _token => uint256
-    getPotBalance: CallFn<[number, string], number>,
+  // uint256 _potId, address _token => uint256
+  getPotBalance: CallFn<[number, string], number>;
 
-    // uint256 => bytes32, bytes32, bool, bool, uint256, uint256, uint256, uint256, uint256, uint256[]
-    getTask: CallFn<
-      [number],
-      [
-        string,
-        string,
-        boolean,
-        boolean,
-        number,
-        number,
-        number,
-        number,
-        number,
-        [number], // Currently just one item for skills
-      ],
-    >,
+  // uint256 => bytes32, bytes32, bool, bool, uint256, uint256, uint256, uint256, uint256, uint256[]
+  getTask: CallFn<
+    [number],
+    [
+      string,
+      string,
+      boolean,
+      boolean,
+      number,
+      number,
+      number,
+      number,
+      number,
+      [number], // Currently just one item for skills
+    ],
+  >;
 
-    // => uint256
-    getTaskCount: CallFn<null, number>,
+  // => uint256
+  getTaskCount: CallFn<null, number>;
 
-    // uint256 _id, uint256 _idx => uint256
-    getTaskDomain: CallFn<[number, number], number>,
+  // uint256 _id, uint256 _idx => uint256
+  getTaskDomain: CallFn<[number, number], number>;
 
-    // uint256 _id, uint256 _idx => uint256
-    getTaskSkill: CallFn<[number, number], number>,
+  // uint256 _id, uint256 _idx => uint256
+  getTaskSkill: CallFn<[number, number], number>;
 
-    // uint256 _id, uint256 _role, address _token => uint256
-    getTaskPayout: CallFn<[number, number, string], number>,
+  // uint256 _id, uint256 _role, address _token => uint256
+  getTaskPayout: CallFn<[number, number, string], number>;
 
-    // uint256 _id, uint8 _idx => address, bool, uint8
-    getTaskRole: CallFn<[number, number], [string, boolean, number]>,
+  // uint256 _id, uint8 _idx => address, bool, uint8
+  getTaskRole: CallFn<[number, number], [string, boolean, number]>;
 
-    // uint256 _id => uint256, uint256
-    getTaskWorkRatings: CallFn<[number], [number, number]>,
+  // uint256 _id => uint256, uint256
+  getTaskWorkRatings: CallFn<[number], [number, number]>;
 
-    // uint256 _id, uint8 _role => bytes32
-    getTaskWorkRatingSecret: CallFn<[number, number], string>,
+  // uint256 _id, uint8 _role => bytes32
+  getTaskWorkRatingSecret: CallFn<[number, number], string>;
 
-    // => address
-    getToken: CallFn<null, string>,
+  // => address
+  getToken: CallFn<null, string>;
 
-    // => uint256
-    getTransactionCount: CallFn<[string], number>,
+  // => uint256
+  getTransactionCount: CallFn<[string], number>;
 
-    addDomain: SendFn<$PropertyType<sendFns, 'addDomain'>>,
-    approveTaskChange: SendFn<$PropertyType<sendFns, 'approveTaskChange'>>,
-    assignWorkRating: SendFn<$PropertyType<sendFns, 'assignWorkRating'>>,
-    cancelTask: SendFn<$PropertyType<sendFns, 'cancelTask'>>,
-    claimColonyFunds: SendFn<$PropertyType<sendFns, 'claimColonyFunds'>>,
-    claimPayout: SendFn<$PropertyType<sendFns, 'claimPayout'>>,
-    finalizeTask: SendFn<$PropertyType<sendFns, 'finalizeTask'>>,
-    makeTask: SendFn<$PropertyType<sendFns, 'makeTask'>>,
-    mintTokens: SendFn<$PropertyType<sendFns, 'mintTokens'>>,
-    mintTokensForColonyNetwork: SendFn<
-      $PropertyType<sendFns, 'mintTokensForColonyNetwork'>,
-    >,
-    moveFundsBetweenPots: SendFn<
-      $PropertyType<sendFns, 'moveFundsBetweenPots'>,
-    >,
-    revealTaskWorkRating: SendFn<
-      $PropertyType<sendFns, 'revealTaskWorkRating'>,
-    >,
-    setTaskBrief: SendFn<$PropertyType<sendFns, 'setTaskBrief'>>,
-    setTaskDomain: SendFn<$PropertyType<sendFns, 'setTaskDomain'>>,
-    setTaskDueDate: SendFn<$PropertyType<sendFns, 'setTaskDueDate'>>,
-    setTaskEvaluatorPayout: SendFn<
-      $PropertyType<sendFns, 'setTaskEvaluatorPayout'>,
-    >,
-    setTaskManagerPayout: SendFn<
-      $PropertyType<sendFns, 'setTaskManagerPayout'>,
-    >,
-    setTaskRoleUser: SendFn<$PropertyType<sendFns, 'setTaskRoleUser'>>,
-    setTaskSkill: SendFn<$PropertyType<sendFns, 'setTaskSkill'>>,
-    setTaskWorkerPayout: SendFn<$PropertyType<sendFns, 'setTaskWorkerPayout'>>,
-    submitTaskDeliverable: SendFn<
-      $PropertyType<sendFns, 'submitTaskDeliverable'>,
-    >,
-    submitTaskWorkRating: SendFn<
-      $PropertyType<sendFns, 'submitTaskWorkRating'>,
-    >,
-    addGlobalSkill: SendFn<$PropertyType<sendFns, 'addGlobalSkill'>>,
-    proposeTaskChange: SendFn<$PropertyType<sendFns, 'proposeTaskChange'>>,
-  };
+  addDomain: SendFn<$PropertyType<sendFns, 'addDomain'>>;
+  approveTaskChange: SendFn<$PropertyType<sendFns, 'approveTaskChange'>>;
+  assignWorkRating: SendFn<$PropertyType<sendFns, 'assignWorkRating'>>;
+  cancelTask: SendFn<$PropertyType<sendFns, 'cancelTask'>>;
+  claimColonyFunds: SendFn<$PropertyType<sendFns, 'claimColonyFunds'>>;
+  claimPayout: SendFn<$PropertyType<sendFns, 'claimPayout'>>;
+  finalizeTask: SendFn<$PropertyType<sendFns, 'finalizeTask'>>;
+  makeTask: SendFn<$PropertyType<sendFns, 'makeTask'>>;
+  mintTokens: SendFn<$PropertyType<sendFns, 'mintTokens'>>;
+  mintTokensForColonyNetwork: SendFn<
+    $PropertyType<sendFns, 'mintTokensForColonyNetwork'>,
+  >;
+  moveFundsBetweenPots: SendFn<$PropertyType<sendFns, 'moveFundsBetweenPots'>>;
+  revealTaskWorkRating: SendFn<$PropertyType<sendFns, 'revealTaskWorkRating'>>;
+  setTaskBrief: SendFn<$PropertyType<sendFns, 'setTaskBrief'>>;
+  setTaskDomain: SendFn<$PropertyType<sendFns, 'setTaskDomain'>>;
+  setTaskDueDate: SendFn<$PropertyType<sendFns, 'setTaskDueDate'>>;
+  setTaskEvaluatorPayout: SendFn<
+    $PropertyType<sendFns, 'setTaskEvaluatorPayout'>,
+  >;
+  setTaskManagerPayout: SendFn<$PropertyType<sendFns, 'setTaskManagerPayout'>>;
+  setTaskRoleUser: SendFn<$PropertyType<sendFns, 'setTaskRoleUser'>>;
+  setTaskSkill: SendFn<$PropertyType<sendFns, 'setTaskSkill'>>;
+  setTaskWorkerPayout: SendFn<$PropertyType<sendFns, 'setTaskWorkerPayout'>>;
+  submitTaskDeliverable: SendFn<
+    $PropertyType<sendFns, 'submitTaskDeliverable'>,
+  >;
+  submitTaskWorkRating: SendFn<$PropertyType<sendFns, 'submitTaskWorkRating'>>;
+  addGlobalSkill: SendFn<$PropertyType<sendFns, 'addGlobalSkill'>>;
+  proposeTaskChange: SendFn<$PropertyType<sendFns, 'proposeTaskChange'>>;
 }
--- packages/colony-js-client/src/interface/ColonyNetworkContract.js
+++ packages/colony-js-client/src/interface/ColonyNetworkContract.js
@@ -1,12 +1,6 @@
 /* @flow */
 
-import type {
-  IContract,
-  CallFn,
-  InterfaceFn,
-  EstimateFn,
-  SendFn,
-} from '@colony/colony-js-adapter';
+import type { IContract, CallFn, SendFn } from '@colony/colony-js-adapter';
 
 type sendFns = {
   // bytes32 _name, address _tokenAddress
@@ -23,39 +17,22 @@ type sendFns = {
 };
 
 export interface ColonyNetworkContract extends IContract {
-  interface: {
-    functions: {
-      // XXX until https://github.com/facebook/flow/issues/3237 is fixed, we can't type these more
-      createColony: InterfaceFn<*>,
-      deposit: InterfaceFn<*>,
-      upgradeColony: InterfaceFn<*>,
-      withdraw: InterfaceFn<*>,
-    },
-  };
-  estimate: {
-    createColony: EstimateFn<$PropertyType<sendFns, 'createColony'>>,
-    deposit: EstimateFn<$PropertyType<sendFns, 'deposit'>>,
-    upgradeColony: EstimateFn<$PropertyType<sendFns, 'upgradeColony'>>,
-    withdraw: EstimateFn<$PropertyType<sendFns, 'withdraw'>>,
-  };
-  functions: {
-    getChildSkillId: CallFn<[number, number], number>,
-    getColony: CallFn<[string], string>,
-    getColonyAt: CallFn<[number], string>,
-    getColonyCount: CallFn<null, number>,
-    getColonyVersionResolver: CallFn<[number], string>,
-    getCurrentColonyVersion: CallFn<null, number>,
-    getParentSkillId: CallFn<[number, number], number>,
-    getReputationUpdateLogEntry: CallFn<
-      [number],
-      [string, number, number, string, number, number],
-    >,
-    getReputationUpdateLogLength: CallFn<null, number>,
-    getSkill: CallFn<[number], [number, number]>,
-    getSkillCount: CallFn<null, number>,
-    createColony: SendFn<$PropertyType<sendFns, 'createColony'>>,
-    deposit: SendFn<$PropertyType<sendFns, 'deposit'>>,
-    upgradeColony: SendFn<$PropertyType<sendFns, 'upgradeColony'>>,
-    withdraw: SendFn<$PropertyType<sendFns, 'withdraw'>>,
-  };
+  getChildSkillId: CallFn<[number, number], number>;
+  getColony: CallFn<[string], string>;
+  getColonyAt: CallFn<[number], string>;
+  getColonyCount: CallFn<null, number>;
+  getColonyVersionResolver: CallFn<[number], string>;
+  getCurrentColonyVersion: CallFn<null, number>;
+  getParentSkillId: CallFn<[number, number], number>;
+  getReputationUpdateLogEntry: CallFn<
+    [number],
+    [string, number, number, string, number, number],
+  >;
+  getReputationUpdateLogLength: CallFn<null, number>;
+  getSkill: CallFn<[number], [number, number]>;
+  getSkillCount: CallFn<null, number>;
+  createColony: SendFn<$PropertyType<sendFns, 'createColony'>>;
+  deposit: SendFn<$PropertyType<sendFns, 'deposit'>>;
+  upgradeColony: SendFn<$PropertyType<sendFns, 'upgradeColony'>>;
+  withdraw: SendFn<$PropertyType<sendFns, 'withdraw'>>;
 }
