b6eff7ca4060b98df0dec25815aa19f673caf6d3
James Lefrère <james@colony.io>
2018-04-23 14:08:45 +0200

    Consolidate flow types

---
+++ packages/colony-js-contract-client/src/flowtypes.js
@@ -0,0 +1,56 @@
+/* @flow */
+
+import type {
+  EventHandlers,
+  Transaction,
+  TransactionOptions,
+  TransactionReceipt,
+} from '@colony/colony-js-adapter';
+
+import ContractClient from './classes/ContractClient';
+
+export type ParamTypes = 'number' | 'string' | 'address' | 'boolean';
+
+export type ParamTypePair = [string, ParamTypes];
+
+export type ParamTypePairs = Array<ParamTypePair>;
+
+export type CallOptions = { timeoutMs: number };
+
+export type SendOptions = {
+  events?: EventHandlers,
+  estimate: boolean,
+  miningTimeoutMs: number,
+  timeoutMs: number,
+  waitForMining: boolean,
+} & TransactionOptions;
+
+export type ContractResponseMeta = {
+  transaction: Transaction,
+  receipt?: TransactionReceipt,
+  receiptPromise?: Promise<TransactionReceipt>,
+};
+
+export type ContractResponse<EventData> = {
+  eventData?: EventData,
+  eventDataPromise?: Promise<EventData>,
+  meta: ContractResponseMeta,
+};
+
+export type ContractMethodArgs<IContractClient: ContractClient<any>> = {
+  client: IContractClient,
+  functionName: string,
+  input: ParamTypePairs,
+  output?: ParamTypePairs,
+};
+
+export type ContractMethodSenderArgs<IContractClient: ContractClient<any>> = {
+  eventHandlers?: EventHandlers,
+} & ContractMethodArgs<IContractClient>;
+
+export type ContractMethodDef<IContractClient: ContractClient<any>> = {
+  client: IContractClient,
+  functionName?: string,
+  input: ParamTypePairs,
+  output?: ParamTypePairs,
+};
