95616ad339a49bbdc0ae15b2ca38510f75182cf7
James Lefrère <james@colony.io>
2018-05-18 14:51:30 +0200

    Update getColony methods
    * Update `getColonyClientByAddress` to use the new constructor
    * Adjust `getColonyAddress` error handling to reflect a null address
    * Use `assert` to check inputs
    * Add test coverage for `getColonyClient`-related functions
    * Add test coverage for `createToken`

---
+++ packages/colony-js-client/src/__tests__/ColonyNetworkClient.test.js
@@ -0,0 +1,178 @@
+/* eslint-env jest */
+
+import { utf8ToHex } from 'web3-utils';
+import createSandbox from 'jest-sandbox';
+import ColonyNetworkClient from '../ColonyNetworkClient';
+import ColonyClient from '../ColonyClient';
+
+jest.mock('web3-utils', () => ({
+  utf8ToHex: jest.fn().mockImplementation(input => input),
+}));
+
+describe('ColonyNetworkClient', () => {
+  const sandbox = createSandbox();
+
+  const colonyAddress = '0x123 Colony Lane';
+  const contract = {};
+  const deployTransaction = {
+    hash: 'the deploy transaction hash',
+  };
+  const transaction = {
+    hash: 'the transaction hash',
+  };
+  const receipt = {
+    contractAddress: 'the contract receipt',
+  };
+  const wallet = {
+    sendTransaction: sandbox.fn().mockImplementation(async () => transaction),
+  };
+  const adapter = {
+    getContract: sandbox.fn().mockImplementation(async () => contract),
+    getContractDeployTransaction: sandbox
+      .fn()
+      .mockImplementation(async () => deployTransaction),
+    getTransactionReceipt: sandbox.fn().mockImplementation(async () => receipt),
+    wallet,
+  };
+
+  beforeEach(() => {
+    sandbox.clear();
+    utf8ToHex.mockClear();
+  });
+
+  test('Creating a token', async () => {
+    const networkClient = new ColonyNetworkClient({ adapter });
+    await networkClient.init();
+
+    const name = 'TestCoin';
+    const symbol = 'TST';
+    const decimals = 20;
+
+    const tokenAddress = await networkClient.createToken({
+      name,
+      symbol,
+      decimals,
+    });
+
+    expect(tokenAddress).toBe(receipt.contractAddress);
+    expect(utf8ToHex).toHaveBeenCalledWith(name);
+    expect(utf8ToHex).toHaveBeenCalledWith(symbol);
+    expect(adapter.getContractDeployTransaction).toHaveBeenCalledWith(
+      expect.objectContaining({ contractName: 'Token' }),
+      [name, symbol, decimals],
+    );
+    expect(wallet.sendTransaction).toHaveBeenCalledWith(deployTransaction);
+    expect(adapter.getTransactionReceipt).toHaveBeenCalledWith(
+      transaction.hash,
+    );
+  });
+
+  test('Getting a ColonyClient', async () => {
+    const networkClient = new ColonyNetworkClient({ adapter });
+    await networkClient.init();
+
+    const colonyClient = {
+      contract: {
+        address: colonyAddress,
+      },
+    };
+    sandbox
+      .spyOn(networkClient, 'getColonyAddress')
+      .mockImplementation(async () => colonyAddress);
+    sandbox
+      .spyOn(networkClient, 'getColonyClientByAddress')
+      .mockImplementation(async () => colonyClient);
+
+    // Ensure that the input is validated
+    try {
+      await networkClient.getColonyClient();
+    } catch (error) {
+      expect(error.toString()).toMatch('Either `key` or `id` must be provided');
+    }
+
+    const id = 23;
+    expect(await networkClient.getColonyClient({ id })).toEqual(colonyClient);
+    expect(networkClient.getColonyAddress).toHaveBeenCalledWith({ id });
+    expect(networkClient.getColonyClientByAddress).toHaveBeenCalledWith(
+      colonyAddress,
+    );
+  });
+
+  test('Getting a ColonyClient by address', async () => {
+    const networkClient = new ColonyNetworkClient({ adapter });
+    await networkClient.init();
+
+    const Mock = sandbox.fn((...args) => new ColonyClient(...args));
+    const colonyClientSpy = sandbox
+      .spyOn(ColonyNetworkClient, 'ColonyClient', 'get')
+      .mockReturnValue(Mock);
+    sandbox.spyOn(ColonyClient.prototype, 'init');
+
+    const colonyClient = await networkClient.getColonyClientByAddress(
+      colonyAddress,
+    );
+
+    expect(colonyClientSpy).toHaveBeenCalled();
+    expect(colonyClient).toBeInstanceOf(ColonyClient);
+    expect(Mock).toHaveBeenCalledWith(
+      {
+        adapter: networkClient.adapter,
+        networkClient,
+      },
+      {
+        contractAddress: colonyAddress,
+      },
+    );
+    expect(colonyClient.init).toHaveBeenCalled();
+  });
+
+  test('Getting a Colony address', async () => {
+    const networkClient = new ColonyNetworkClient({ adapter });
+    await networkClient.init();
+
+    const getColonyByKeySpy = sandbox
+      .spyOn(networkClient.getColonyByKey, 'call')
+      .mockImplementation(async () => ({ address: colonyAddress }));
+    const getColonyByIdSpy = sandbox
+      .spyOn(networkClient.getColonyById, 'call')
+      .mockImplementation(async () => ({ address: colonyAddress }));
+
+    const key = 'My Colony';
+    const id = 24;
+
+    // Ensure that the input is validated
+    try {
+      await networkClient.getColonyAddress();
+      expect(true).toBe(false); // should be unreachable
+    } catch (error) {
+      expect(error.toString()).toMatch('Either `key` or `id` must be provided');
+    }
+
+    // Getting the the address by id
+    expect(await networkClient.getColonyAddress({ id })).toBe(colonyAddress);
+    expect(getColonyByIdSpy).toHaveBeenCalledWith({ id });
+    expect(getColonyByKeySpy).not.toHaveBeenCalled();
+    getColonyByIdSpy.mockReset();
+    getColonyByKeySpy
+      .mockReset()
+      .mockImplementation(async () => ({ address: colonyAddress }));
+
+    // Getting the the address by key
+    expect(await networkClient.getColonyAddress({ key })).toBe(colonyAddress);
+    expect(getColonyByIdSpy).not.toHaveBeenCalled();
+    expect(getColonyByKeySpy).toHaveBeenCalledWith({ key });
+    getColonyByIdSpy.mockReset();
+    getColonyByKeySpy.mockReset();
+
+    // No address found
+    getColonyByKeySpy.mockImplementation(async () => ({ address: null }));
+    try {
+      await networkClient.getColonyAddress({ key });
+      expect(true).toBe(false); // should be unreachable
+    } catch (error) {
+      expect(error.toString()).toMatch(
+        `Colony with key ${key} could not be found`,
+      );
+    }
+  });
+});
