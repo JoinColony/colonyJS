654ecbc5cc68314eb984aae077cefbe46746de8a
James Lefrère <james@colony.io>
2018-05-26 11:31:29 +0200

    Implement new param types
    * Move old imported functions (`validate, getMethodArgs, getMethodReturnValue`) to `ContractMethod`
    * Simplify performing validation, converting input/output values with the param types map
    * Add `getValidatedArgs` public method

--- packages/colony-js-contract-client/src/classes/ContractMethod.js
+++ packages/colony-js-contract-client/src/classes/ContractMethod.js
@@ -1,11 +1,18 @@
 /* @flow */
 
-import ContractClient from './ContractClient';
-import getMethodArgs from '../modules/getMethodArgs';
-import getMethodReturnValue from '../modules/getMethodReturnValue';
-import validate from '../modules/validate';
+import isPlainObject from 'lodash.isplainobject';
+import { makeAssert } from '@colony/colony-js-utils';
 
 import type { ContractMethodArgs, ParamTypePairs } from '../flowtypes';
+import ContractClient from './ContractClient';
+import {
+  validateValue,
+  convertInputValue,
+  convertOutputValue,
+} from '../modules/paramTypes';
+import { areParamPairsEmpty, isInputEmpty } from '../modules/inputValidation';
+
+const assertValid = makeAssert('Validation failed');
 
 /**
  * Abstract class for interacting with contract methods.
@@ -20,11 +27,6 @@ export default class ContractMethod<
   input: ParamTypePairs;
   output: ParamTypePairs;
 
-  // Imported functions
-  _validate = validate;
-  _getMethodArgs = getMethodArgs;
-  _getMethodReturnValue = getMethodReturnValue;
-
   constructor({
     client,
     functionName,
@@ -36,34 +38,104 @@ export default class ContractMethod<
     this.functionName = functionName;
     if (output) this.output = output;
   }
+
   /**
-   * Given named input values, validate them against the expected parameters
-   * for this method, throwing errors if validation fails.
+   * Given the result of a contract method call, and the input values used to
+   * make the call, transform these with the expected output parameters in
+   * order to get named output values as the method's `OutputValues`
+   * The inputValues are included for function overloading.
    */
-  validate(inputValues?: InputValues) {
-    return this._validate(inputValues, this.input);
+  // eslint-disable-next-line no-unused-vars
+  _getOutputValues(callResult: any, inputValues?: InputValues): OutputValues {
+    // It may be a single value or an array; treat it as an array
+    const values = [].concat(callResult);
+
+    // Clean values according to their type and coalesce into an object
+    return this.output && this.output.length
+      ? // $FlowFixMe
+        this.output
+          .map(([name, type], index) => [
+            name,
+            convertOutputValue(values[index], type),
+          ])
+          .reduce(
+            (acc, [name, value]) => Object.assign(acc, { [name]: value }),
+            {},
+          )
+      : callResult;
   }
+
+  /**
+   * Given input values, map them against the method's expected parameters,
+   * with the appropriate conversion for each type.
+   */
+  _parseInputValues(inputValues: InputValues) {
+    return this.input.map(([paramName, paramType]) =>
+      convertInputValue(inputValues[paramName], paramType),
+    );
+  }
+
   /**
    * Given named input values, transform these with the expected parameters
    * in order to get an array of arguments expected by the contract function.
    */
-  getMethodArgs(inputValues?: InputValues) {
-    return this._getMethodArgs(inputValues, this.input);
+  _getMethodArgs(inputValues?: InputValues): Array<any> {
+    let args = [];
+
+    if (inputValues == null) return args;
+
+    if (this.input && this.input.length) {
+      args = this._parseInputValues(inputValues);
+    } else if (
+      isPlainObject(inputValues) &&
+      Object.getOwnPropertyNames(inputValues).length
+    ) {
+      // eslint-disable-next-line no-console
+      console.warn(
+        'Warning: _getMethodArgs called with parameters for a method that ' +
+          'does not accept parameters',
+      );
+    }
+    return args;
   }
-  /**
-   * Given the result of a contract method call, and the input values used to
-   * make the call, transform these with the expected output parameters in
-   * order to get named output values as the method's `ReturnValues`
-   */
-  // eslint-disable-next-line no-unused-vars
-  getOutputValues(callResult: any, inputValues?: InputValues): OutputValues {
-    return this._getMethodReturnValue(callResult, this.output);
-  }
+
   /**
    * Given arguments to call the contract method with, return
    * transaction data as a hex string.
    */
   createTransactionData(callArgs: Array<any>) {
     return this.client.createTransactionData(this.functionName, callArgs);
+  }
+
+  /**
+   * Given input values, validate them and return parsed method args.
+   */
+  getValidatedArgs(inputValues?: any) {
+    this.validate(inputValues);
+    return this._getMethodArgs(inputValues);
+  }
+
+  /**
+   * Given parameters (as an object with named parameters, but potentially any
+   * kind of invalid input), validate each parameter against the expected type
+   * for this method, throwing vaidation errors or returning true.
+   */
+  validate(inputValues?: any = {}): boolean {
+    if (areParamPairsEmpty(this.input) && isInputEmpty(inputValues))
+      return true;
+
+    assertValid(isPlainObject(inputValues), 'Expected parameters as an object');
+
+    assertValid(
+      Object.getOwnPropertyNames(inputValues).length === this.input.length,
+      'Mismatching parameters/method parameters sizes',
+    );
+
+    return this.input.every(([paramName, paramType]) =>
+      assertValid(
+        Boolean(validateValue(inputValues[paramName], paramType)),
+        `Parameter "${paramName}" expected a value of type "${paramType}"`,
+      ),
+    );
   }
 }
