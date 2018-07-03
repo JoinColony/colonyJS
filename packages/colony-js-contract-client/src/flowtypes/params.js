9d7e7ae07d67dba75f5ba6e8cec37170d91b2599
James Lefrère <james@colony.io>
2018-07-03 12:21:12 +0200

    Split up flowtypes

---
+++ packages/colony-js-contract-client/src/flowtypes/params.js
@@ -0,0 +1,22 @@
+/* @flow */
+
+export type ParamTypes =
+  | 'address'
+  | 'bigNumber'
+  | 'boolean'
+  | 'date'
+  | 'hexString'
+  | 'ipfsHash'
+  | 'number'
+  | 'tokenAddress'
+  | 'string';
+
+// [param name, param type, default value (optional)]
+export type Param = [string, ParamTypes, *];
+export type Params = Array<Param>;
+
+export type ParamTypeDef = {
+  validate: (value: any) => boolean,
+  convertOutput: (value: any) => *,
+  convertInput: (value: any) => *,
+};
