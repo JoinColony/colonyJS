259d21edbefe1ec3263068ea432c77993146c097
Ryan Christoffersen <ryan@ryanchristo.com>
2018-08-24 20:22:03 +0700

    Add react/webpack configuration

---
+++ sdk/packages/colony-starter-react/client/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+import './index.scss'
+
+render(
+  <div>
+    <h1>
+      {'colony-starter-react'}
+    </h1>
+  </div>,
+  document.getElementById('app'),
+)
---
+++ sdk/packages/colony-starter-react/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  target: 'web',
+  entry: {
+    main: path.join(__dirname, '/client/index.jsx'),
+  },
+  output: {
+    filename: 'index.min.js',
+    path: path.join(__dirname, '/public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            query: 'presets[]=env,presets[]=react,presets[]=stage-2',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+}
