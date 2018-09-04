38f103a14e0f0cc087dec3f39ff104a5bb056161
James Lefrère <james@colony.io>
2018-09-04 19:36:44 +0200

    Add TokenClient events

--- packages/colony-js-client/src/TokenClient/index.js
+++ packages/colony-js-client/src/TokenClient/index.js
@@ -7,6 +7,19 @@ import GetTokenInfo from './callers/GetTokenInfo';
 type Address = string;
 
 export default class TokenClient extends ContractClient {
+  events: {
+    Approval: TokenClient.Event<{
+      owner: Address,
+      spender: Address,
+      value: BigNumber,
+    }>,
+    Burn: TokenClient.Event<{ address: Address, amount: BigNumber }>,
+    LogSetAuthority: TokenClient.Event<{ authority: Address }>,
+    LogSetOwner: TokenClient.Event<{ owner: Address }>,
+    Mint: TokenClient.Event<{ address: Address, amount: BigNumber }>,
+    Transfer: TokenClient.Event<{ to: Address, value: BigNumber }>,
+  };
+
   /*
     Get information about the ERC20 token itself
   */
@@ -158,59 +171,16 @@ export default class TokenClient extends ContractClient {
     const destinationAddress = ['destinationAddress', 'address'];
     const user = ['user', 'address'];
 
-    const Transfer = {
-      contract: this.contract,
-      handler({ to, value }: { to: Address, value: BigNumber }) {
-        return {
-          to,
-          value,
-        };
-      },
-    };
-    const Approval = {
-      contract: this.contract,
-      handler({
-        owner,
-        spender,
-        value,
-      }: {
-        owner: Address,
-        spender: Address,
-        value: BigNumber,
-      }) {
-        return {
-          owner,
-          spender,
-          value,
-        };
-      },
-    };
-    const Mint = {
-      contract: this.contract,
-      handler({ guy, wad }: { guy: Address, wad: BigNumber }) {
-        return {
-          address: guy,
-          amount: wad,
-        };
-      },
-    };
-    const Burn = Object.assign({}, Mint);
-    const LogSetOwner = {
-      contract: this.contract,
-      handler({ owner }: { owner: Address }) {
-        return {
-          owner,
-        };
-      },
-    };
-    const LogSetAuthority = {
-      contract: this.contract,
-      handler({ authority }: { authority: Address }) {
-        return {
-          authority,
-        };
-      },
-    };
+    this.addEvent('Transfer', [['to', 'address'], ['value', 'bigNumber']]);
+    this.addEvent('Approval', [
+      ['owner', 'address'],
+      ['spender', 'address'],
+      ['value', 'bigNumber'],
+    ]);
+    this.addEvent('Mint', [['address', 'address'], ['amount', 'bigNumber']]);
+    this.addEvent('Burn', [['address', 'address'], ['amount', 'bigNumber']]);
+    this.addEvent('LogSetOwner', [['owner', 'address']]);
+    this.addEvent('LogSetAuthority', [['authority', 'address']]);
 
     this.getTokenInfo = new GetTokenInfo({ client: this });
 
@@ -233,37 +203,30 @@ export default class TokenClient extends ContractClient {
 
     this.addSender('transfer', {
       input: [destinationAddress, amount],
-      eventHandlers: { Transfer },
     });
 
     this.addSender('transferFrom', {
       input: [sourceAddress, destinationAddress, amount],
-      eventHandlers: { Transfer },
     });
 
     this.addSender('approve', {
       input: [user, amount],
-      eventHandlers: { Approval },
     });
 
     this.addSender('mint', {
       input: [amount],
-      eventHandlers: { Mint },
     });
 
     this.addSender('burn', {
       input: [amount],
-      eventHandlers: { Burn },
     });
 
     this.addSender('setOwner', {
       input: [['owner', 'address']],
-      eventHandlers: { LogSetOwner },
     });
 
     this.addSender('setAuthority', {
       input: [['authority', 'address']],
-      eventHandlers: { LogSetAuthority },
     });
   }
 }
