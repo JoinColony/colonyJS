5936bc173811702b4fac10ffa31d9e7a93f4a2f2
Ryan Christoffersen <ryan@ryanchristo.com>
2018-11-28 21:29:15 +0700

    Update select account component styling

--- sdk/packages/colony-starter-react/client/components/Accounts.jsx
+++ sdk/packages/colony-starter-react/client/components/Accounts.jsx
@@ -8,29 +8,31 @@ const Accounts = ({
   getAccountsSuccess,
   selectAccount,
 }) => (
-  <div>
-    <h2 className={styles.title}>
-      {'Select Account:'}
-    </h2>
-    {getAccountsLoading &&
-      <div className={styles.message}>
-        {'connecting to colonyNetwork...'}
-      </div>
-    }
-    {getAccountsError &&
-      <div className={styles.message}>
-        {getAccountsError}
-      </div>
-    }
-    {accounts && !getAccountsLoading && !getAccountsError &&
-      <ul className={styles.list}>
-        {accounts.map((account, index) => (
-          <li key={index} onClick={() => selectAccount(index)}>
-            {account}
-          </li>
-        ))}
-      </ul>
-    }
+  <div className={styles.container}>
+    <div className={styles.selector}>
+      <h2 className={styles.title}>
+        {'Select Account'}
+      </h2>
+      {getAccountsLoading &&
+        <div className={styles.message}>
+          {'connecting to colonyNetwork...'}
+        </div>
+      }
+      {getAccountsError &&
+        <div className={styles.message}>
+          {getAccountsError}
+        </div>
+      }
+      {accounts && !getAccountsLoading && !getAccountsError &&
+        <ul className={styles.list}>
+          {accounts.map((account, index) => (
+            <li key={index} onClick={() => selectAccount(index)}>
+              {account}
+            </li>
+          ))}
+        </ul>
+      }
+    </div>
   </div>
 )
 
