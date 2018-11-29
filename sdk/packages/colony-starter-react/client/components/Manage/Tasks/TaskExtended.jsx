fade284723027c436cff302282ae6f64ccb21bb1
Ryan Christoffersen <ryan@ryanchristo.com>
2018-11-29 15:40:50 +0700

    Update task view properties

--- sdk/packages/colony-starter-react/client/components/Manage/Tasks/TaskExtended.jsx
+++ sdk/packages/colony-starter-react/client/components/Manage/Tasks/TaskExtended.jsx
@@ -4,8 +4,9 @@ import styles from './TaskExtended.scss'
 const TaskExtended = ({ task }) => (
   <div className={styles.container}>
     <p>{'ID: ' + task.id}</p>
-    <p>{'Skill: ' + task.skillTitle}</p>
-    <p>{'Domain: ' + task.domainTitle}</p>
+    <p>{'Domain ID: ' + task.domainId}</p>
+    <p>{'Skill ID: ' + task.skillId}</p>
+    <p>{'Status: ' + task.status}</p>
     <p>{'Due Date: ' + task.dueDate}</p>
     <p>{'Title: ' + task.specification.title}</p>
     <p>{'Description: ' + task.specification.description}</p>
@@ -16,12 +17,10 @@ const TaskExtended = ({ task }) => (
     <p>{'Manager Payout: ' + task.payouts.manager}</p>
     <p>{'Evaluator Payout: ' + task.payouts.evaluator}</p>
     <p>{'Worker Payout: ' + task.payouts.worker}</p>
-    <p>{'Date Delivered: ' + task.deliverableDate}</p>
+    <p>{'Date Completed: ' + task.completionDate}</p>
     <p>{'Delivered Message: ' + task.deliverable.message}</p>
     <p>{'Ratings Submitted: ' + task.ratings.count}</p>
     <p>{'Last Rating Submitted: ' + task.ratings.date}</p>
-    <p>{'Cancelled: ' + task.cancelled}</p>
-    <p>{'Finalized: ' + task.finalized}</p>
     <p>{'Payouts We Cannot Make: ' + task.payoutsWeCannotMake}</p>
   </div>
 )
--- sdk/packages/colony-starter-react/client/components/Manage/Tasks/TaskPreview.jsx
+++ sdk/packages/colony-starter-react/client/components/Manage/Tasks/TaskPreview.jsx
@@ -4,14 +4,13 @@ import styles from './TaskPreview.scss'
 const TaskPreview = ({ task }) => (
   <div className={styles.container}>
     <p>{'ID: ' + task.id}</p>
-    <p>{'Domain: ' + task.domainTitle}</p>
-    <p>{'Skill: ' + task.skillTitle}</p>
+    <p>{'Domain ID: ' + task.domainId}</p>
+    <p>{'Skill ID: ' + task.skillId}</p>
+    <p>{'Status: ' + task.status}</p>
     <p>{'Due Date: ' + task.dueDate}</p>
     <p>{'Title: ' + task.specification.title}</p>
     <p>{'Description: ' + task.specification.description}</p>
-    <p>{'Date Delivered: ' + task.deliverableDate}</p>
-    <p>{'Cancelled: ' + task.cancelled}</p>
-    <p>{'Finalized: ' + task.finalized}</p>
+    <p>{'Date Completed: ' + task.completionDate}</p>
     <p>{'Payouts We Cannot Make: ' + task.payoutsWeCannotMake}</p>
   </div>
 )
