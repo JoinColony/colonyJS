42742e2c4f9efdeba5cae23ac0894ffe98cf426c
Ryan Christoffersen <ryan@ryanchristo.com>
2019-01-20 23:55:13 +0700

    Update `addDomain` example to latest version

--- sdk/packages/colony-starter-angular/examples/addDomain.js
+++ sdk/packages/colony-starter-angular/examples/addDomain.js
@@ -1,41 +1,28 @@
 // An example using the addDomain method
 const addDomain = async (colonyClient, parentDomainId) => {
 
-    // In order to add a domain to our colony, we will need the local skill id of
-    // the parent domain. Domains are registered as local skills, so we will use
-    // the parent domain id to get the local skill id of the parent domain, which
-    // we will then use as the parent skill id when we add our new domain.
-    const { localSkillId: parentSkillId } = await colonyClient.getDomain.call({
-      domainId: parentDomainId,
-    });
-  
-    // Create a new domain using the local skill id of the parent domain
-    await colonyClient.addDomain.send({
-      parentSkillId: parentDomainId,
-    });
-  
-    // Get the total number of domains in the colony (our new domain id)
-    const { count: domainId } = await colonyClient.getDomainCount.call();
-  
-    // Get the pot id of our new domain
-    const { potId } = await colonyClient.getDomain.call({ domainId });
-  
-    // Check out the logs to see our new domain
-    console.log('Domain:', {
-      id: domainId,
-      parentSkillId,
-      potId,
-    });
-  
-    // Return our new domain
-    return {
-      id: domainId,
-      parentSkillId,
-      potId,
-    };
-  
+  // Create a new domain using the parent domain id
+  await colonyClient.addDomain.send({ parentDomainId });
+
+  // Get the total number of domains in the colony (our new domain id)
+  const { count: domainId } = await colonyClient.getDomainCount.call();
+
+  // Get the pot id of our new domain
+  const domain = await colonyClient.getDomain.call({ domainId });
+
+  // Check out the logs to see our new domain
+  console.log('Domain:', {
+    id: domainId,
+    ...domain,
+  });
+
+  // Return our new domain
+  return {
+    id: domainId,
+    ...domain,
   };
-  
-  // Export addDomain example
-  module.exports = addDomain;
-  
+
+};
+
+// Export addDomain example
+module.exports = addDomain;
