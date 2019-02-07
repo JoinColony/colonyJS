5e19cc983f95c48dec7c38c328027dc112ebe13d
Ryan Christoffersen <ryan@ryanchristo.com>
2019-02-07 14:00:06 +0700

    Add "local" support for `getNetworkLoader`

--- packages/colony-js-client/src/getNetworkClient.js
+++ packages/colony-js-client/src/getNetworkClient.js
@@ -3,19 +3,45 @@
 import { providers } from 'ethers';
 import EthersAdapter from '@colony/colony-js-adapter-ethers';
 import NetworkLoader from '@colony/colony-js-contract-loader-network';
+import { TrufflepigLoader } from '@colony/colony-js-contract-loader-http';
 import ColonyNetworkClient from './ColonyNetworkClient/index';
 
+// This method provides a simple way of getting an initialized network client
+// that uses NetworkLoader for the remote network and TrufflePigLoader for a
+// local testrpc network (TrufflePig must be installed and running).
 const getNetworkClient = async (network: string, wallet: any) => {
-  const loader = new NetworkLoader({ network });
-  const provider = providers.getDefaultProvider(network);
+  let loader;
+  let provider;
+
+  // Set loader and provider
+  if (network === 'local') {
+    loader = new TrufflepigLoader();
+
+    // Throw custom error if TrufflePig is not installed and running
+    loader.getAccounts().catch(() => {
+      throw new Error('Make sure you have TrufflePig installed and running.');
+    });
+
+    provider = new providers.JsonRpcProvider();
+  } else {
+    loader = new NetworkLoader({ network });
+    provider = providers.getDefaultProvider(network);
+  }
+
+  // Support offline wallets (wallets opened with purser)
   if (!wallet.provider) Object.assign(wallet, { provider });
+
+  // Initialize adpaters using ethers
   const adapter = new EthersAdapter({
     loader,
     provider,
     wallet,
   });
+
+  // Initialize network client using ethers adapter and default query
   const networkClient = new ColonyNetworkClient({ adapter, query: {} });
   await networkClient.init();
+
   return networkClient;
 };
 
