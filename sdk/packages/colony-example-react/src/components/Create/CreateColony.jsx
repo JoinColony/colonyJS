9353161bd2d168efc3fcc44ecf11a41d71ee517b
Ryan Christoffersen <ryan@ryanchristo.com>
2019-03-28 20:22:29 +0400

    Refactor react example based on react starter

---
+++ sdk/packages/colony-example-react/src/components/Create/CreateColony.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styles from './CreateColony.module.scss'
+
+const CreateColony = ({
+  createColonyError,
+  createColonyLoading,
+  createColonySuccess,
+  handleChange,
+  handleClick,
+}) => (
+  <div className={styles.container}>
+    <h2>{'Create Colony'}</h2>
+    <div className={styles.field}>
+      <label htmlFor="tokenAddress">
+        {'Token Address:'}
+      </label>
+      <input
+        id="tokenAddress"
+        onChange={handleChange}
+        placeholder="0x0"
+        type="text"
+      />
+    </div>
+    <div className={styles.buttons}>
+      <button onClick={handleClick}>
+        {'Create Colony'}
+      </button>
+    </div>
+    {createColonyError &&
+      <div className={styles.message}>
+        <span className={styles.error}>
+          {createColonyError}
+        </span>
+      </div>
+    }
+    {createColonyLoading &&
+      <div className={styles.message}>
+        <span>
+          {'loading...'}
+        </span>
+      </div>
+    }
+  </div>
+)
+
+export default CreateColony
