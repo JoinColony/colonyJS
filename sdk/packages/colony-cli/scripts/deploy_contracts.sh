3362f456a8fa2af9cec07aa10300b7ea25b2b96b
Ryan Christoffersen <ryan@ryanchristo.com>
2019-03-30 23:05:07 +0400

    Allow deploy-contracts to specify version

--- sdk/packages/colony-cli/scripts/deploy_contracts.sh
+++ sdk/packages/colony-cli/scripts/deploy_contracts.sh
@@ -1,7 +1,44 @@
 #!/bin/bash
 
+# Paths
+ROOT_PATH=$(pwd)
+
+# Log
+log() {
+  CYAN='\033[0;36m'
+  NONE='\033[0m'
+  echo "${CYAN}$1${NONE}"
+}
+
 # Move to colonyNetwork directory
 cd lib/colonyNetwork
+
+# If version specified
+if [ $1 ]; then
+
+  # Set colonyNework version
+  log "Checking out colonyNetwork version..."
+  git -c advice.detachedHead=false checkout $1
+
+else
+
+  # Set colonyNework version
+  log "Checking out colonyNetwork version..."
+  git -c advice.detachedHead=false checkout temp/eth-denver
+
+fi
+
+# Install colonyNetwork dependencies
+log "Installing colonyNetwork dependencies..."
+yarn
+
+# Initialize colonyNetwork submodules
+log "Initializing colonyNetwork submodules..."
+git submodule update --init --recursive
+
+# Provision colonyNetwork submodules
+log "Provisioning colonyNetwork submodules..."
+yarn run provision:token:contracts
 
 # Compile and deploy colonyNetwork contracts
 ./node_modules/.bin/truffle migrate --compile-all --reset
