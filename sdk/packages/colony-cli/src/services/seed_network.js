cb532986d4e0be5cabfec7c37b7b8c6898f6885f
Ryan Christoffersen <ryan@ryanchristo.com>
2019-04-02 12:51:52 +0400

    Update seed network script in cli

--- sdk/packages/colony-cli/src/services/seed_network.js
+++ sdk/packages/colony-cli/src/services/seed_network.js
@@ -1,66 +1,28 @@
-// import prerequisites and colony client
-const { providers, Wallet } = require('ethers');
-const { default: EthersAdapter } = require('@colony/colony-js-adapter-ethers');
-const { TrufflepigLoader } = require('@colony/colony-js-contract-loader-http');
-const { default: ColonyNetworkClient } = require('@colony/colony-js-client');
+// Import the prerequisites
+const { getNetworkClient } = require('@colony/colony-js-client');
+const { open } = require('@colony/purser-software');
 
-// create instance of Trufflepig loader
-const loader = new TrufflepigLoader();
-
-// create provider for local TestRPC (Ganache)
-const provider = new providers.JsonRpcProvider('http://localhost:8545/');
-
-// seedNetwork
-
+// Run script
 (async () => {
 
-  // get private key from test account
+  // Get private key from first test account
   const { privateKey } = await loader.getAccount(0);
 
-  // create a wallet with the private key
-  const wallet = new Wallet(privateKey, provider);
+  // Get colony network client
+  const networkClient = await getNetworkClient('local', wallet);
 
-  // create an ethers powered adapter
-  const adapter = new EthersAdapter({
-    loader,
-    provider,
-    wallet,
-  });
-
-  // connect to ColonyNetwork with adapter
-  const networkClient = new ColonyNetworkClient({ adapter });
-
-  // initialize netwrok client
-  await networkClient.init();
-
-  // seed global skills
-  await seedGlobalSkills(networkClient);
-
-  // exit process
-  process.exit();
-
-})();
-
-// seedGlobalSkills
-
-const seedGlobalSkills = async (networkClient) => {
-
-  // get skill count
-  const { count: skillCount } = await networkClient.getSkillCount.call();
-
-  // get meta colony client
+  // Get meta colony client
   const metaColonyClient = await networkClient.getMetaColonyClient();
 
-  // add global skill
+  // Add global skill
   await metaColonyClient.addGlobalSkill.send({ parentSkillId: 1 });
 
-  // add global skill
+  // Add global skill
   await metaColonyClient.addGlobalSkill.send({ parentSkillId: 1 });
 
-  // add global skill
+  // Add global skill
   await metaColonyClient.addGlobalSkill.send({ parentSkillId: 1 });
 
-  // return true
-  return true;
-
-}
+})()
+  .then(() => process.exit())
+  .catch(err => console.error(err));
