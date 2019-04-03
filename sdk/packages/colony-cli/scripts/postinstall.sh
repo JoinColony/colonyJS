9e841fd7aad7e5e1dacb6b135f1fe60b624c1571
Ryan Christoffersen <ryan@ryanchristo.com>
2019-04-04 00:40:35 +0400

    Remove unecessary logs in postinstall script

--- sdk/packages/colony-cli/scripts/postinstall.sh
+++ sdk/packages/colony-cli/scripts/postinstall.sh
@@ -1,41 +1,34 @@
 #!/bin/bash
 
-# Log
-log() {
-  CYAN="\033[0;36m"
-  NONE="\033[0m"
-  echo "${CYAN}$1${NONE}"
-}
-
 # Check colonyNetwork directory
 if [ ! -d "lib/colonyNetwork" ]; then
 
   # Pull docker image
-  log "Pulling docker image..."
   docker pull ethereum/solc:0.4.23
 
   # Create and move to lib directory
   mkdir lib && cd lib
 
-  # Clone colonyNetwork repository
-  log "Cloning colonyNetwork repository..."
-
   # Check CI variable
   if [ -z $CI ]; then
+
+    # Clone colonyNetwork repository using ssh
     git clone ssh://git@github.com/JoinColony/colonyNetwork.git
+
   else
+
+    # Clone colonyNetwork repository using https
     git clone https://github.com/JoinColony/colonyNetwork
+
   fi
 
   # Move to colonyNetwork directory
   cd colonyNetwork
 
   # Checkout default colonyNetwork version
-  log "Checking out colonyNetwork version..."
   git checkout 543423abb133119a4029ac2adcc8cebb16a8c6d9
 
   # Install colonyNetwork dependencies
-  log "Installing colonyNetwork dependencies..."
   yarn
 
 fi
