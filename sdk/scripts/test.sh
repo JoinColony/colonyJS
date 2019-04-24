7f508892db201e9c205fa530b805297c49bc2fd5
Ryan Christoffersen <ryan@ryanchristo.com>
2019-04-24 12:34:48 +0200

    Test packages independently

---
+++ sdk/scripts/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yarn --cwd packages/colony-example test
+yarn --cwd packages/colony-example-angular test
+yarn --cwd packages/colony-example-react test
+yarn --cwd packages/colony-starter test
+yarn --cwd packages/colony-starter-angular test
+yarn --cwd packages/colony-starter-contract test
+yarn --cwd packages/colony-starter-react test
