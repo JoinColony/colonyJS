96f1db53f5cd6b0ed5e6dc977860685e7e2a58e1
Ryan Christoffersen <ryan@ryanchristo.com>
2019-05-17 22:55:12 +0200

    Update colony setup script

--- sdk/packages/colony-cli/src/scripts/colonySetup.js
+++ sdk/packages/colony-cli/src/scripts/colonySetup.js
@@ -6,9 +6,6 @@ const { BN } = require('web3-utils');
 // Set the private key (this is the private key for the first Ganache test account)
 const privateKey = '0x0355596cdb5e5242ad082c4fe3f8bbe48c9dba843fe1f99dd8272f487e70efae';
 
-// Set contract address for OneTxPayment contract
-const OneTxPayment = '0xA8DA163375713753Acc7e1D429c64F72b9412077';
-
 // Run setup
 (async () => {
 
@@ -81,20 +78,6 @@ const OneTxPayment = '0xA8DA163375713753Acc7e1D429c64F72b9412077';
 
   // Check out the logs to see the colony pot balance after claiming funds
   console.log('Colony Pot Balance: ' + potBalance.balance);
-
-  // Set the admin role for OneTxPayment
-  await colonyClient.setAdminRole.send({
-    user: OneTxPayment,
-  });
-
-  // Check if the contract was assigned the admin role
-  const { hasRole } = await colonyClient.hasUserRole.call({
-    user: OneTxPayment,
-    role: 'ADMIN',
-  })
-
-  // Check out the logs to see if the contract was assigned the admin role
-  console.log('OneTxPayment:', hasRole);
 
 })()
   .then(() => process.exit())
