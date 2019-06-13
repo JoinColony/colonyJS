28c77b265e6458676ba8e43d6f416f5f1a37ad3a
gtarsia <guidotarsia@gmail.com>
2019-06-13 16:18:50 -0300

    Replace `.apply` with spread

--- sdk/packages/colony-example-vue/src/helpers/send-proxy.js
+++ sdk/packages/colony-example-vue/src/helpers/send-proxy.js
@@ -3,7 +3,7 @@ export default function sendProxy(network) {
   const handler = {
     get(obj, prop) {
       return async function networkDelegate(...args) {
-        const result = await obj[prop].send.apply(obj[prop], args)
+        const result = await obj[prop].send(...args)
         const { successful } = result
         if (!successful) {
           console.log(`${prop} error: `, 'font-weight: bold', result)
