a2e52b18dcd9441f93ea267ce26422e3c33bc54e
gtarsia <guidotarsia@gmail.com>
2019-06-17 15:22:07 -0300

    Remove router-view from vue-starter

--- sdk/packages/colony-starter-vue/src/App.vue
+++ sdk/packages/colony-starter-vue/src/App.vue
@@ -7,11 +7,19 @@
       </span>
     </header>
     <div class="content">
-      <router-view/>
+      <Home />
     </div>
   </div>
 </template>
+<script>
+import Home from '@/views/Home.vue'
 
+export default {
+  components: {
+    Home,
+  },
+}
+</script>
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
