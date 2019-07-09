e43f00e449620dba1fc2de9f66363e08b60b41cb
Ryan Christoffersen <ryan@ryanchristo.com>
2019-07-09 13:28:21 +0200

    Update dependencies and lockfile

--- sdk/scripts/test.sh
+++ sdk/scripts/test.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-yarn --cwd packages/colony-example test
-yarn --cwd packages/colony-example-angular test
-yarn --cwd packages/colony-example-react test
+echo 'Testing packages/colony-starter...'
 yarn --cwd packages/colony-starter test
+
+echo 'Testing packages/colony-starter-angular...'
 yarn --cwd packages/colony-starter-angular test
+
+echo 'Testing packages/colony-starter-contract...'
 yarn --cwd packages/colony-starter-contract test
+
+echo 'Testing packages/colony-starter-react...'
 yarn --cwd packages/colony-starter-react test
+
+echo 'Testing packages/colony-example...'
+yarn --cwd packages/colony-example test
+
+echo 'Testing packages/colony-example-angular...'
+yarn --cwd packages/colony-example-angular test
+
+echo 'Testing packages/colony-example-react...'
+yarn --cwd packages/colony-example-react test
