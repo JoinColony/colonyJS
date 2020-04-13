06454336c4074b4d681c04f9102de5f22dbce66e
Christian Maniewski <code@chmanie.com>
2020-04-13 14:47:36 +0200

    Expose necessary functionality

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 // @TODO export a few types here that we might need
 
-export { default as ColonyNetworkClient } from './clients/ColonyNetworkClient';
+export { default as getColonyNetworkClient } from './clients/ColonyNetworkClient';
+export * from './constants';
+export * from '../versions';
