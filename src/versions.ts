601554356c56be180047b6731b53cc99a0307104
Christian Maniewski <code@chmanie.com>
2020-04-13 15:35:02 +0200

    Remove versions file

--- src/versions.ts
+++
@@ -1,9 +0,0 @@
-// Versions of colonyNetwork. TS automatically increments from 1
-export enum ColonyVersion {
-  GoerliGlider = 1, // glider-rc.1
-  Glider,
-  AuburnGlider,
-  BurgundyGlider,
-}
-
-export const CurrentVersion = ColonyVersion.BurgundyGlider;
