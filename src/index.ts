740de90777f2388e84333aadc78d7c2e9bb72079
Christian Maniewski <code@chmanie.com>
2020-06-08 18:15:07 +0200

    Export all ContractClients

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,27 @@ import type {
   AnyColonyClient,
   ExtendedIColonyNetwork,
 } from './clients/ColonyNetworkClient';
+import { ExtendedOneTxPaymentDeployer } from './clients/OneTxPaymentDeployerClient';
+import { ExtendedOneTxPayment } from './clients/OneTxPaymentClient';
 import type { TransactionOverrides as TxOverrides } from './contracts/1';
 
 export { default as getColonyNetworkClient } from './clients/ColonyNetworkClient';
 export { default as getTokenClient } from './clients/TokenClient';
+
 export * from './constants';
 export * from './helpers';
 
 export type ColonyClient = AnyColonyClient;
 export type NetworkClient = ExtendedIColonyNetwork;
 export type TokenClient = Token;
-export type ContractClient = ColonyClient | NetworkClient | TokenClient;
+export type OneTxPaymentFactoryClient = ExtendedOneTxPaymentDeployer;
+export type OneTxPaymentClient = ExtendedOneTxPayment;
+export type ContractClient =
+  | ColonyClient
+  | NetworkClient
+  | OneTxPaymentClient
+  | OneTxPaymentFactoryClient
+  | TokenClient;
 export type TransactionOverrides = TxOverrides;
 
 /* eslint-enable import/first */
