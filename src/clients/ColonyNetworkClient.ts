35f26a07ffcb1e3f120e65de639395d2379a9701
Christian Maniewski <code@chmanie.com>
2020-08-27 12:03:22 +0200

    Add docs for getColonyNetworkClient

--- src/clients/ColonyNetworkClient.ts
+++ src/clients/ColonyNetworkClient.ts
@@ -116,6 +116,31 @@ interface NetworkClientOptions {
   reputationOracleEndpoint?: string;
 }
 
+/**
+ * The main entry point for accessing the deployed colonyNetwork contracts
+ *
+ * Specify a network and an ethers compatible singer or provider to get back an
+ * initialized and extended (ethers) contract client for the colonyNetwork. From
+ * here you can access different colonies, extensions, ENS and other features of Colony.
+ *
+ * Example
+ * ```ts
+ * import { getColonyNetworkClient, Network } = from '@colony/colony-js';
+ * import { JsonRpcProvider } from 'ethers/providers';
+ *
+ * // For local connections (run an Ethereum node on port 8545);
+ * const provider = new JsonRpcProvider();
+ *
+ * // Just for reading data - to sign transactions we need to pass in a signer.
+ * const networkClient = await getColonyNetworkClient(Network.Mainnet, provider);
+ * ```
+ *
+ * @param network One of the available options. See [[Network]].
+ * @param signerOrProvider An [ethers](https://github.com/ethers-io/ethers.js/)
+ * compatible signer or provider instance
+ * @param options Here you can supply options for accessing certain contracts
+ * (mostly used in local/dev environments)
+ */
 const getColonyNetworkClient = (
   network: Network = Network.Mainnet,
   signerOrProvider: Signer | Provider,
