b91eaa8d270fc682eb06bd05b182d325fc40974b
Christian Maniewski <code@chmanie.com>
2020-08-27 12:03:05 +0200

    Export CoinMachine clients

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ import { ColonyClientV4 } from './clients/Colony/ColonyClientV4';
 import { ColonyClientV5 } from './clients/Colony/ColonyClientV5';
 import { OneTxPaymentFactoryClient } from './clients/OneTxPaymentDeployerClient';
 import { OneTxPaymentClient } from './clients/OneTxPaymentClient';
+import { CoinMachineFactoryClient } from './clients/CoinMachineDeployerClient';
+import { CoinMachineClient } from './clients/CoinMachineClient';
 import { TokenClient } from './clients/TokenClient';
 import { TokenLockingClient } from './clients/TokenLockingClient';
 
@@ -19,6 +21,8 @@ export { ColonyClientV3 } from './clients/Colony/ColonyClientV3';
 export { ColonyClientV4 } from './clients/Colony/ColonyClientV4';
 export { OneTxPaymentFactoryClient } from './clients/OneTxPaymentDeployerClient';
 export { OneTxPaymentClient } from './clients/OneTxPaymentClient';
+export { CoinMachineFactoryClient } from './clients/CoinMachineDeployerClient';
+export { CoinMachineClient } from './clients/CoinMachineClient';
 export {
   TokenClient,
   ColonyTokenClient,
@@ -41,6 +45,8 @@ export type ColonyClient =
   | ColonyClientV5;
 
 export type ContractClient =
+  | CoinMachineClient
+  | CoinMachineFactoryClient
   | ColonyClient
   | ColonyNetworkClient
   | OneTxPaymentClient
