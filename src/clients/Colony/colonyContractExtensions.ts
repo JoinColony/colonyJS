ec0f88ef0fbd4d70e6b8b4e249e277d86b94d8bc
Raul Glogovetan <raul@glogovetan.com>
2021-03-11 15:50:55 +0200

    Add: `VotingReputation` client

--- src/clients/Colony/colonyContractExtensions.ts
+++ src/clients/Colony/colonyContractExtensions.ts
@@ -2,18 +2,22 @@ import getCoinMachineClient, { CoinMachineClient } from '../CoinMachineClient';
 import getOneTxPaymentClient, {
   OneTxPaymentClient,
 } from '../OneTxPaymentClient';
+import getVotingReputationClient, {
+  VotingReputationClient,
+} from '../VotingReputationClient';
 
 export enum Extension {
   CoinMachine = 'CoinMachine',
   // FundingQueue = 'FundingQueue',
   OneTxPayment = 'OneTxPayment',
-  // VotingReputation = 'VotingReputation',
+  VotingReputation = 'VotingReputation',
 }
 
 // All of the valid extensions
 export type ExtensionClients = {
   [Extension.CoinMachine]: CoinMachineClient;
   [Extension.OneTxPayment]: OneTxPaymentClient;
+  [Extension.VotingReputation]: VotingReputationClient;
 };
 
 export type ExtensionClient = ExtensionClients[keyof ExtensionClients];
@@ -22,6 +26,7 @@ export type ExtensionClient = ExtensionClients[keyof ExtensionClients];
 export const extensionFactoryMap = {
   [Extension.CoinMachine]: getCoinMachineClient,
   [Extension.OneTxPayment]: getOneTxPaymentClient,
+  [Extension.VotingReputation]: getVotingReputationClient,
 };
 
 export const extensions = Object.keys(extensionFactoryMap);
