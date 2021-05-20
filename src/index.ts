0db6afc0228b171f6ec2072e1f7b22681ec783b3
Raul Glogovetan <raul@glogovetan.com>
2021-05-20 11:28:28 +0300

    Refactor: extension clients export

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,20 @@ export { ColonyClientV3 } from './clients/Colony/ColonyClientV3';
 export { ColonyClientV4 } from './clients/Colony/ColonyClientV4';
 export { ColonyClientV5 } from './clients/Colony/ColonyClientV5';
 export { ColonyClientV6 } from './clients/Colony/ColonyClientV6';
-export { OneTxPaymentClient } from './clients/Extensions/OneTxPaymentClient';
-export { CoinMachineClient } from './clients/Extensions/CoinMachineClient';
-export { VotingReputationClient } from './clients/Extensions/VotingReputationClient';
+export {
+  OneTxPaymentClient,
+  OneTxPaymentClient as OneTxPaymentClientV1,
+} from './clients/Extensions/OneTxPayment/1/OneTxPaymentClient';
+export { CoinMachineClient as CoinMachineClientV1 } from './clients/Extensions/CoinMachine/1/CoinMachineClient';
+export {
+  CoinMachineClient,
+  CoinMachineClient as CoinMachineClientV2,
+} from './clients/Extensions/CoinMachine/2/CoinMachineClient';
+export { VotingReputationClient as VotingReputationClientV1 } from './clients/Extensions/VotingReputation/1/VotingReputationClient';
+export {
+  VotingReputationClient,
+  VotingReputationClient as VotingReputationClientV2,
+} from './clients/Extensions/VotingReputation/2/VotingReputationClient';
 export {
   TokenClient,
   ColonyTokenClient,
