4ef22279ba1f30ad2e4f65a5a26ef83d455b9f3a
Raul Glogovetan <raul@glogovetan.com>
2021-10-17 01:39:37 +0300

    Add: incipient IColony events types

--- src/types.ts
+++ src/types.ts
@@ -52,3 +52,27 @@ export interface ReputationOracleAllSkillsResponse {
 export interface ReputationOracleAllMembersResponse {
   addresses: string[];
 }
+
+/*
+ * @TODO Complete this list of events enum
+ *
+ * A good idea would be type it dynamically from the latest IColony ABI
+ * Something like this:
+ * ```
+ * function strEnum<T extends string>(o: Array<T>): {[K in T]: K} {
+ *   return o.reduce((res, key) => {
+ *     res[key] = key;
+ *     return res;
+ *   }, Object.create(null));
+ * }
+ * let dynamicArrayJSON = [ 'RED', 'BLUE', 'GREEN' ]
+ * const Colors = strEnum( dynamicArrayJSON )
+ * ```
+ * To make this work properly you'll need to do the same thing we do with Colony
+ * Clients and use the `getAllAbiEvents` function to get events from all versions
+ * of the contracts
+ */
+export enum IColonyEvents {
+  ColonyRoleSet = 'ColonyRoleSet',
+  RecoveryRoleSet = 'RecoveryRoleSet',
+}
