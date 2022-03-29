3927f04bdb081bcd531f021f29c0fc38720cbc64
chmanie <code@chmanie.com>
2022-03-29 18:30:26 +0200

    Fix all lints

--- sdk/examples/browser/src/basic.ts
+++ sdk/examples/browser/src/basic.ts
@@ -7,11 +7,14 @@ const provider = new providers.JsonRpcProvider('https://xdai.colony.io/rpc2/');
 
 // Get the Colony's XDAI funding in the ROOT pot (id 1)
 const getColonyFunding = async (colonyAddress: string) => {
-    const colonyNetworkClient = getColonyNetworkClient(Network.Xdai, provider);
-    const colonyClient = await colonyNetworkClient.getColonyClient(colonyAddress);
-    const funding = await colonyClient.getFundingPotBalance(Id.RootPot, CLNY.Xdai);
-    return formatEther(funding);
-}
+  const colonyNetworkClient = getColonyNetworkClient(Network.Xdai, provider);
+  const colonyClient = await colonyNetworkClient.getColonyClient(colonyAddress);
+  const funding = await colonyClient.getFundingPotBalance(
+    Id.RootPot,
+    CLNY.Xdai,
+  );
+  return formatEther(funding);
+};
 
 // Just some basic setup to display the UI
 const addressInput: HTMLInputElement = document.querySelector('#address');
@@ -19,24 +22,33 @@ const button = document.querySelector('#button');
 const errElm: HTMLParagraphElement = document.querySelector('#error');
 const resultElm: HTMLParagraphElement = document.querySelector('#result');
 
-const panik = (err: string) => errElm.innerText = err;
-const kalm = () => errElm.innerText = '';
-const speak = (msg: string) => resultElm.innerText = msg;
+const panik = (err: string) => {
+  errElm.innerText = err;
+};
+const kalm = () => {
+  errElm.innerText = '';
+};
+const speak = (msg: string) => {
+  resultElm.innerText = msg;
+};
 
 button.addEventListener('click', async () => {
-    kalm();
-    const colonyAddress = addressInput.value;
-    if (!isAddress(colonyAddress)) {
-        return panik('This is not a valid address');
-    }
-    speak('Thinking...');
-    addressInput.value = '';
-    let funding: string;
-    try {
-        funding = await getColonyFunding(colonyAddress)
-        speak(`${funding} XDAI in root domain of Colony with address: ${colonyAddress}`);
-    } catch (e) {
-        panik(`Found an error: ${e.message}`)
-        speak('');
-    }
+  kalm();
+  const colonyAddress = addressInput.value;
+  if (!isAddress(colonyAddress)) {
+    return panik('This is not a valid address');
+  }
+  speak('Thinking...');
+  addressInput.value = '';
+  let funding: string;
+  try {
+    funding = await getColonyFunding(colonyAddress);
+    speak(
+      `${funding} XDAI in root domain of Colony with address: ${colonyAddress}`,
+    );
+  } catch (e) {
+    panik(`Found an error: ${e.message}`);
+    speak('');
+  }
+  return null;
 });
--- sdk/examples/node/index.ts
+++ sdk/examples/node/index.ts
@@ -7,11 +7,18 @@ const provider = new providers.JsonRpcProvider('https://rpc.gnosischain.com/');
 
 // Get the Colony's XDAI funding in the ROOT pot (id 1)
 const start = async () => {
-    const colonyNetworkClient = getColonyNetworkClient(Network.Xdai, provider);
-    const metaColonyClient = await colonyNetworkClient.getMetaColonyClient()
-    const funding = await metaColonyClient.getFundingPotBalance(Id.RootPot, CLNY.Xdai);
-    const { address } = metaColonyClient;
-    console.info(`${formatEther(funding)} XDAI in root domain of MetaColony with address: ${address}`);
-}
+  const colonyNetworkClient = getColonyNetworkClient(Network.Xdai, provider);
+  const metaColonyClient = await colonyNetworkClient.getMetaColonyClient();
+  const funding = await metaColonyClient.getFundingPotBalance(
+    Id.RootPot,
+    CLNY.Xdai,
+  );
+  const { address } = metaColonyClient;
+  console.info(
+    `${formatEther(
+      funding,
+    )} XDAI in root domain of MetaColony with address: ${address}`,
+  );
+};
 
 start();
