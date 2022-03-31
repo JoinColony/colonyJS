897ec6fbc092710a7a5dd4f45274756178c773d3
chmanie <code@chmanie.com>
2022-03-31 13:13:13 +0200

    Add OneTxPaymentClientV3

--- src/clients/Extensions/OneTxPayment/exports.ts
+++ src/clients/Extensions/OneTxPayment/exports.ts
@@ -11,13 +11,20 @@ import getOneTxPaymentClientV1, {
 import getOneTxPaymentClientV2, {
   OneTxPaymentClient as OneTxPaymentClient2,
 } from './OneTxPaymentClientV2';
+import getOneTxPaymentClientV3, {
+  OneTxPaymentClient as OneTxPaymentClient3,
+} from './OneTxPaymentClientV3';
 
-const ONE_TX_PAYMENT_VERSION_NEXT = 3;
+const ONE_TX_PAYMENT_VERSION_NEXT = 4;
 
 export type OneTxPaymentClientV1 = OneTxPaymentClient1;
 export type OneTxPaymentClientV2 = OneTxPaymentClient2;
+export type OneTxPaymentClientV3 = OneTxPaymentClient3;
 
-export type AnyOneTxPaymentClient = OneTxPaymentClient1 | OneTxPaymentClient2;
+export type AnyOneTxPaymentClient =
+  | OneTxPaymentClient1
+  | OneTxPaymentClient2
+  | OneTxPaymentClient3;
 
 export const ONE_TX_PAYMENT_VERSIONS = createContractVersionArray(
   ONE_TX_PAYMENT_VERSION_NEXT,
@@ -31,6 +38,7 @@ export const oneTxPaymentIncompatibilityMap: Record<
 > = {
   1: [],
   2: [],
+  3: [],
 };
 
 /** @internal */
@@ -44,6 +52,8 @@ export const getOneTxPaymentClient = (
       return getOneTxPaymentClientV1(colonyClient, address);
     case 2:
       return getOneTxPaymentClientV2(colonyClient, address);
+    case 3:
+      return getOneTxPaymentClientV3(colonyClient, address);
     default:
       return assertExhaustiveSwitch(
         version,
