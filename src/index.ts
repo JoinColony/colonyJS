a522f65a8063abce3bec4caa612cdd0c2a5e6330
chmanie <code@chmanie.com>
2022-06-02 23:01:33 +0200

    Export TokenLockingClient, restructure exports

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,29 @@
-export type { ColonyDataTypes } from './contracts/IColony/exports';
+export { default as getColonyNetworkClient } from './clients/ColonyNetworkClient';
+
+export { formatColonyRoles } from './utils';
 
 export * from './constants';
 export * from './helpers';
-export * from './types';
-
-export { formatColonyRoles } from './utils';
 
 // Entry points and types for Core and Extensions
 export * from './clients/Core/exports';
 export * from './clients/Extensions/exports';
 
-export {
+// Export some types
+export type { ColonyDataTypes } from './contracts/IColony/exports';
+
+export type {
   ColonyNetworkClient,
-  default as getColonyNetworkClient,
   NetworkClientOptions,
 } from './clients/ColonyNetworkClient';
 
-export {
+export type { TokenLockingClient } from './clients/TokenLockingClient';
+
+export type {
   TokenClient,
   ColonyTokenClient,
   Erc20TokenClient,
   DaiTokenClient,
 } from './clients/TokenClient';
+
+export * from './types';
