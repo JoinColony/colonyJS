4e53e45e14e2877cf26f09df41227ef61d00d0bb
chmanie <code@chmanie.com>
2023-02-16 17:05:46 +0100

    Export extras events from a new endpoint

---
+++ src/extras/events.ts
@@ -0,0 +1,44 @@
+export * from '../contracts/events';
+
+// We need to manually export those as they are ambiguous as they exist in multiple event contracts
+export {
+  MetaTransactionExecutedEvent,
+  MetaTransactionExecutedEventFilter,
+  MetaTransactionExecutedEventObject,
+  RecoveryModeEnteredEvent,
+  RecoveryRoleSetEvent,
+  RecoveryModeExitedEventObject,
+  RecoveryRoleSetEventFilter,
+  RecoveryStorageSlotSetEvent,
+  RecoveryRoleSetEventObject,
+  RecoveryStorageSlotSetEventFilter,
+  RecoveryModeExitedEventFilter,
+  RecoveryModeExitApprovedEventObject,
+  RecoveryModeEnteredEventFilter,
+  RecoveryModeExitedEvent,
+  RecoveryModeEnteredEventObject,
+  RecoveryModeExitApprovedEventFilter,
+  RecoveryModeExitApprovedEvent,
+  RecoveryStorageSlotSetEventObject,
+} from '../contracts/events/IColonyNetworkEvents';
+
+// Again, all ColonyExtensions have those so we export them from the OG contract
+export {
+  ExtensionInitialisedEvent,
+  LogSetAuthorityEventObject,
+  LogSetOwnerEvent,
+  LogSetOwnerEventFilter,
+  LogSetAuthorityEventFilter,
+  ExtensionInitialisedEventObject,
+  LogSetAuthorityEvent,
+  ExtensionInitialisedEventFilter,
+  LogSetOwnerEventObject,
+} from '../contracts/ColonyExtension';
+
+export * from '../contracts/events/CoinMachineEvents';
+export * from '../contracts/events/IColonyEvents';
+export * from '../contracts/events/IColonyNetworkEvents';
+export * from '../contracts/events/MetaTxTokenEvents';
+export * from '../contracts/events/OneTxPaymentEvents';
+export * from '../contracts/events/VotingReputationEvents';
+export * from '../contracts/events/WhitelistEvents';
--- src/extras/index.ts
+++ src/extras/index.ts
@@ -1,50 +1,5 @@
 // Export useful types (for Colony SDK, always the latest version)
-export type { ColonyDataTypes } from '../contracts/IColony/10/IColony';
-export type { VotingReputationDataTypes } from '../contracts/IVotingReputation/7/IVotingReputation';
+export type { ColonyDataTypes } from '../contracts/IColony/12/IColony';
+export type { VotingReputationDataTypes } from '../contracts/IVotingReputation/8/IVotingReputation';
 
 export * from '../contracts';
-
-// We need to manually export those as they are ambiguous as they exist in multiple event contracts
-export {
-  MetaTransactionExecutedEvent,
-  MetaTransactionExecutedEventFilter,
-  MetaTransactionExecutedEventObject,
-  RecoveryModeEnteredEvent,
-  RecoveryRoleSetEvent,
-  RecoveryModeExitedEventObject,
-  RecoveryRoleSetEventFilter,
-  RecoveryStorageSlotSetEvent,
-  RecoveryRoleSetEventObject,
-  RecoveryStorageSlotSetEventFilter,
-  RecoveryModeExitedEventFilter,
-  RecoveryModeExitApprovedEventObject,
-  RecoveryModeEnteredEventFilter,
-  RecoveryModeExitedEvent,
-  RecoveryModeEnteredEventObject,
-  RecoveryModeExitApprovedEventFilter,
-  RecoveryModeExitApprovedEvent,
-  RecoveryStorageSlotSetEventObject,
-} from '../contracts/events/IColonyNetworkEvents';
-
-// Again, all ColonyExtensions have those so we export them from the OG contract
-export {
-  ExtensionInitialisedEvent,
-  LogSetAuthorityEventObject,
-  LogSetOwnerEvent,
-  LogSetOwnerEventFilter,
-  LogSetAuthorityEventFilter,
-  ExtensionInitialisedEventObject,
-  LogSetAuthorityEvent,
-  ExtensionInitialisedEventFilter,
-  LogSetOwnerEventObject,
-} from '../contracts/ColonyExtension';
-
-export * from '../tokens/contracts/TokenLocking';
-
-export * from '../contracts/events/CoinMachineEvents';
-export * from '../contracts/events/IColonyEvents';
-export * from '../contracts/events/IColonyNetworkEvents';
-export * from '../contracts/events/MetaTxTokenEvents';
-export * from '../contracts/events/OneTxPaymentEvents';
-export * from '../contracts/events/VotingReputationEvents';
-export * from '../contracts/events/WhitelistEvents';
