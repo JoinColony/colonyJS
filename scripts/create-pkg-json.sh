23c7080daf0a37fdfc4ca846441a1326d6f2faf9
chmanie <code@chmanie.com>
2023-06-27 13:20:25 +0200

    chore: Fix changeset config for fake package.jsons

--- scripts/create-pkg-json.sh
+++ scripts/create-pkg-json.sh
@@ -3,6 +3,8 @@
 cat >"$PWD/dist/cjs/package.json" <<!EOF
 {
     "name": "colony-typescript-for-build-only",
+    "private": true,
+    "version": "0.0.0",
     "type": "commonjs"
 }
 !EOF
@@ -10,6 +12,8 @@ cat >"$PWD/dist/cjs/package.json" <<!EOF
 cat >"$PWD/dist/esm/package.json" <<!EOF
 {
     "name": "colony-typescript-for-build-only",
+    "private": true,
+    "version": "0.0.0",
     "type": "module"
 }
 !EOF
