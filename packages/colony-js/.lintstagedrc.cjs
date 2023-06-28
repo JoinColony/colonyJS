7edf89b07eb5b1c64ec599b3adbed141a74ea481
chmanie <code@chmanie.com>
2023-06-28 13:27:49 +0200

    maintenance: Exclude sdk and colony-js from build

--- packages/colony-js/.lintstagedrc.cjs
+++ packages/colony-js/.lintstagedrc.cjs
@@ -1,10 +1,6 @@
-const { resolve } = require('path');
-
 module.exports = {
   '**/*.ts?(x)': (filenames) => [
     `eslint --cache --fix ${filenames.join(' ')}`,
     'tsc -p tsconfig.json --noEmit',
-    'npm run build-docs',
-    `git add ${resolve(__dirname, 'docs', 'api')}`
   ],
 }
