44dcea87f9c57d4d2329b777fb91ba45f485ccc1
chmanie <code@chmanie.com>
2023-09-05 18:41:39 +0200

    Add tsup for faster sdk compilation

---
+++ packages/sdk/tsup.min.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  clean: true,
+  entry: ['src/index.ts'],
+  format: 'esm',
+  minify: true,
+  noExternal: [/@colony\/.+/],
+  outDir: 'dist/prod',
+  outExtension() {
+    return {
+      js: '.min.js',
+    };
+  },
+  target: 'es2022',
+  tsconfig: 'tsconfig.build.json',
+});
