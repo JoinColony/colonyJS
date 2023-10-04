37e3b511ea67ac89a56ba18321ed83fdaddd4889
chmanie <code@chmanie.com>
2023-10-04 14:54:59 +0200

    Fix tests for colony-js, sdk

--- packages/colony-js/jest.config.cjs
+++ packages/colony-js/jest.config.cjs
@@ -2,6 +2,7 @@ const { defaults } = require('ts-jest/presets');
 
 module.exports = {
   ...defaults,
+  rootDir: 'src',
   resolver: 'ts-jest-resolver',
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'd.ts', 'tsx', 'node'],
 };
