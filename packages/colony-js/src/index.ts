b1a0ff2a39e0c26658391cc475128e1b3dc52674
chmanie <code@chmanie.com>
2023-10-04 19:31:58 +0200

    Expose necessary exports

--- packages/colony-js/src/index.ts
+++ packages/colony-js/src/index.ts
@@ -1,10 +1,16 @@
 export type { ContractReceipt } from 'ethers';
 
+export const LATEST_TAG = 'glwss4';
+
 export * from '@colony/core';
+export * from '@colony/tokens';
+export { ClientType as ClientTypeTokens } from '@colony/tokens';
 
 export { default as getColonyNetworkClient } from './clients/ColonyNetworkClient.js';
 
 export * from './constants.js';
+export { ClientType } from './constants.js';
+
 export * from './helpers.js';
 export * from './types.js';
 
