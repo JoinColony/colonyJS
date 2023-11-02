faca5e31b71528175f40aaca990a40a6858372bc
chmanie <code@chmanie.com>
2023-11-02 14:07:28 +0100

    colony-js: Export popular contract factories

--- packages/colony-js/src/index.ts
+++ packages/colony-js/src/index.ts
@@ -1,12 +1,13 @@
-export type { ContractReceipt } from 'ethers';
-
+// Attention: always adjust to newest versions!
 export const LATEST_TAG = 'glwss4';
+export { IColony__factory as ColonyFactory } from './contracts/IColony/14/index.js';
 
 export * from '@colony/core';
 export * from '@colony/tokens';
 export { ClientType as ClientTypeTokens } from '@colony/tokens';
 
 export { default as getColonyNetworkClient } from './clients/ColonyNetworkClient.js';
+export { IColonyNetwork__factory as ColonyNetworkFactory } from './contracts/index.js';
 
 export * from './constants.js';
 export { ClientType } from './constants.js';
@@ -18,9 +19,12 @@ export * from './types.js';
 export * from './clients/Core/exports.js';
 export * from './clients/Extensions/exports.js';
 
+// Export some types
 export type {
   ColonyNetworkClient,
   NetworkClientOptions,
 } from './clients/ColonyNetworkClient.js';
 
-export { IBasicMetaTransaction } from './contracts/index.js';
+export type { ContractReceipt } from 'ethers';
+
+export type { IBasicMetaTransaction } from './contracts/index.js';
