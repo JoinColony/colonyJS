d0880b080bb87b53cf122925c4fde250cb130332
Jakub Zajac <jakub@colony.io>
2024-03-18 21:37:27 +0000

    Fix: `create-pkg-json.sh` script on OS X

--- scripts/create-pkg-json.sh
+++ scripts/create-pkg-json.sh
@@ -3,7 +3,15 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cp "$SCRIPT_DIR/../packages/__templates/package.json" "$PWD/dist/cjs/package.json"
-sed -i 's/{{MODULE_TYPE}}/commonjs/g' "$PWD/dist/cjs/package.json"
+if [[ $(uname) == "Darwin" ]]; then
+  sed -i '' 's/{{MODULE_TYPE}}/commonjs/g' "$PWD/dist/cjs/package.json"
+else
+  sed -i 's/{{MODULE_TYPE}}/commonjs/g' "$PWD/dist/cjs/package.json"
+fi
 
 cp "$SCRIPT_DIR/../packages/__templates/package.json" "$PWD/dist/esm/package.json"
-sed -i 's/{{MODULE_TYPE}}/module/g' "$PWD/dist/esm/package.json"
+if [[ $(uname) == "Darwin" ]]; then
+  sed -i '' 's/{{MODULE_TYPE}}/module/g' "$PWD/dist/esm/package.json"
+else
+  sed -i 's/{{MODULE_TYPE}}/module/g' "$PWD/dist/esm/package.json"
+fi
