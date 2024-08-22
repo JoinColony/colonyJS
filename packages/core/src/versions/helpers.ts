6028525f28a73da52a3394fa60fe64a68770106b
Dave Creaser <david.creaser@outlook.com>
2024-08-22 11:05:16 +0100

    feat: Add helper to get extension required colony version

--- packages/core/src/versions/helpers.ts
+++ packages/core/src/versions/helpers.ts
@@ -1,38 +1,7 @@
 import { Extension } from '../constants.js';
-import { assertExhaustiveSwitch } from '../utils/index.js';
+import { COLONY_VERSIONS } from './IColony.js';
 import { ColonyVersion, ExtensionVersion } from './index.js';
-import {
-  FundingQueueVersion,
-  fundingQueueIncompatibilityMap,
-} from './FundingQueue.js';
-import {
-  VotingReputationVersion,
-  votingReputationIncompatibilityMap,
-} from './VotingReputation.js';
-import {
-  OneTxPaymentVersion,
-  oneTxPaymentIncompatibilityMap,
-} from './OneTxPayment.js';
-import {
-  ReputationBootstrapperVersion,
-  reputationBootstrapperIncompatibilityMap,
-} from './ReputationBootstrapper.js';
-import {
-  StagedExpenditureVersion,
-  stagedExpenditureIncompatibilityMap,
-} from './StagedExpenditure.js';
-import {
-  StakedExpenditureVersion,
-  stakedExpenditureIncompatibilityMap,
-} from './StakedExpenditure.js';
-import {
-  StreamingPaymentsVersion,
-  streamingPaymentsIncompatibilityMap,
-} from './StreamingPayments.js';
-import {
-  TokenSupplierVersion,
-  tokenSupplierIncompatibilityMap,
-} from './TokenSupplier.js';
+import { getExtensionIncompatibilityMap } from './utils.js';
 
 /**
  * Checks the compatibility of an extension version with a colony version it requests to be installed in
@@ -48,71 +17,41 @@ export const isExtensionCompatible = (
   extension: Extension,
   extensionVersion: ExtensionVersion,
   colonyVersion: ColonyVersion,
-) => {
-  switch (extension) {
-    case Extension.FundingQueue: {
-      const map =
-        fundingQueueIncompatibilityMap[extensionVersion as FundingQueueVersion];
-      return !!map && !map.includes(colonyVersion);
-    }
-    case Extension.IVotingReputation: {
-      const map =
-        votingReputationIncompatibilityMap[
-          extensionVersion as VotingReputationVersion
-        ];
-      return !!map && !map.includes(colonyVersion);
-    }
-    case Extension.OneTxPayment: {
-      const map =
-        oneTxPaymentIncompatibilityMap[extensionVersion as OneTxPaymentVersion];
-      return !!map && !map.includes(colonyVersion);
-    }
-    case Extension.ReputationBootstrapper: {
-      const map =
-        reputationBootstrapperIncompatibilityMap[
-          extensionVersion as ReputationBootstrapperVersion
-        ];
-      return !!map && !map.includes(colonyVersion);
-    }
-    case Extension.StagedExpenditure: {
-      const map =
-        stagedExpenditureIncompatibilityMap[
-          extensionVersion as StagedExpenditureVersion
-        ];
-      return !!map && !map.includes(colonyVersion);
-    }
-    case Extension.StakedExpenditure: {
-      const map =
-        stakedExpenditureIncompatibilityMap[
-          extensionVersion as StakedExpenditureVersion
-        ];
-      return !!map && !map.includes(colonyVersion);
-    }
-    case Extension.StreamingPayments: {
-      const map =
-        streamingPaymentsIncompatibilityMap[
-          extensionVersion as StreamingPaymentsVersion
-        ];
-      return !!map && !map.includes(colonyVersion);
-    }
-    case Extension.TokenSupplier: {
-      const map =
-        tokenSupplierIncompatibilityMap[
-          extensionVersion as TokenSupplierVersion
-        ];
-      return !!map && !map.includes(colonyVersion);
-    }
-    case Extension.VotingReputation: {
-      const map =
-        votingReputationIncompatibilityMap[
-          extensionVersion as VotingReputationVersion
-        ];
-      return !!map && !map.includes(colonyVersion);
-    }
-    default:
-      return assertExhaustiveSwitch(
-        extension,
-        'Could not find extension with name',
-      );
+): boolean => {
+  const map = getExtensionIncompatibilityMap(extension, extensionVersion);
+
+  return !!map && !map.includes(colonyVersion);
+};
+
+/**
+ * Calculates and returns the lowest compatible colony version for a given version of an extension
+ *
+ * @param extension - A valid `Extension` contract name
+ * @param extensionVersion - The version of the extension to check
+ *
+ * @returns the lowest compatible colony version for a given version of an extension
+ */
+export const getExtensionLowestCompatibleColonyVersion = (
+  extension: Extension,
+  extensionVersion: ExtensionVersion,
+): ColonyVersion => {
+  const map = getExtensionIncompatibilityMap(extension, extensionVersion);
+
+  if (!map) {
+    throw new Error(
+      `Could not find incompatibility map for extension "${extension}" with version: ${extensionVersion}`,
+    );
   }
+
+  const colonyVersion = COLONY_VERSIONS.find(
+    (v) => v === map[map.length - 1] + 1,
+  );
+
+  if (colonyVersion === undefined || null) {
+    throw new Error(
+      `No compatible colony version found for extension "${extension}" with version: ${extensionVersion}`,
+    );
+  }
+
+  return colonyVersion;
 };
