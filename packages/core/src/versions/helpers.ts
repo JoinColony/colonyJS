2621e983400ed9a430c9cacff2e994859a193657
Dave Creaser <david.creaser@outlook.com>
2024-08-22 11:32:32 +0100

    Fix imports by moving util

--- packages/core/src/versions/helpers.ts
+++ packages/core/src/versions/helpers.ts
@@ -1,7 +1,97 @@
-import { Extension } from '../constants.js';
 import { COLONY_VERSIONS } from './IColony.js';
 import { ColonyVersion, ExtensionVersion } from './index.js';
-import { getExtensionIncompatibilityMap } from './utils.js';
+import { assertExhaustiveSwitch } from '../utils/index.js';
+import {
+  fundingQueueIncompatibilityMap,
+  FundingQueueVersion,
+} from './FundingQueue.js';
+import {
+  oneTxPaymentIncompatibilityMap,
+  OneTxPaymentVersion,
+} from './OneTxPayment.js';
+import {
+  reputationBootstrapperIncompatibilityMap,
+  ReputationBootstrapperVersion,
+} from './ReputationBootstrapper.js';
+import {
+  stagedExpenditureIncompatibilityMap,
+  StagedExpenditureVersion,
+} from './StagedExpenditure.js';
+import {
+  stakedExpenditureIncompatibilityMap,
+  StakedExpenditureVersion,
+} from './StakedExpenditure.js';
+import {
+  streamingPaymentsIncompatibilityMap,
+  StreamingPaymentsVersion,
+} from './StreamingPayments.js';
+import {
+  tokenSupplierIncompatibilityMap,
+  TokenSupplierVersion,
+} from './TokenSupplier.js';
+import {
+  votingReputationIncompatibilityMap,
+  VotingReputationVersion,
+} from './VotingReputation.js';
+import { Extension } from '../constants.js';
+
+const getExtensionIncompatibilityMap = (
+  extension: Extension,
+  extensionVersion: ExtensionVersion,
+) => {
+  switch (extension) {
+    case Extension.FundingQueue: {
+      return fundingQueueIncompatibilityMap[
+        extensionVersion as FundingQueueVersion
+      ];
+    }
+    case Extension.IVotingReputation: {
+      return votingReputationIncompatibilityMap[
+        extensionVersion as VotingReputationVersion
+      ];
+    }
+    case Extension.OneTxPayment: {
+      return oneTxPaymentIncompatibilityMap[
+        extensionVersion as OneTxPaymentVersion
+      ];
+    }
+    case Extension.ReputationBootstrapper: {
+      return reputationBootstrapperIncompatibilityMap[
+        extensionVersion as ReputationBootstrapperVersion
+      ];
+    }
+    case Extension.StagedExpenditure: {
+      return stagedExpenditureIncompatibilityMap[
+        extensionVersion as StagedExpenditureVersion
+      ];
+    }
+    case Extension.StakedExpenditure: {
+      return stakedExpenditureIncompatibilityMap[
+        extensionVersion as StakedExpenditureVersion
+      ];
+    }
+    case Extension.StreamingPayments: {
+      return streamingPaymentsIncompatibilityMap[
+        extensionVersion as StreamingPaymentsVersion
+      ];
+    }
+    case Extension.TokenSupplier: {
+      return tokenSupplierIncompatibilityMap[
+        extensionVersion as TokenSupplierVersion
+      ];
+    }
+    case Extension.VotingReputation: {
+      return votingReputationIncompatibilityMap[
+        extensionVersion as VotingReputationVersion
+      ];
+    }
+    default:
+      return assertExhaustiveSwitch(
+        extension,
+        'Could not find extension with name',
+      );
+  }
+};
 
 /**
  * Checks the compatibility of an extension version with a colony version it requests to be installed in
