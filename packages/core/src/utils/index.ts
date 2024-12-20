c00e58e025d29cce5acf49332f7a9e23a3f4b96c
Chris Maniewski <code@chmanie.com>
2024-12-20 04:21:13 +0100

    feat: create wagmi abis with contractor

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,9 +1,9 @@
-/** @internal */
+/** Make sure to use all possible options in a switch statement */
 export const assertExhaustiveSwitch = (x: never, msg: string): never => {
   throw new Error(`${msg}: ${x}`);
 };
 
-/** @internal */
+/** Use this to filter empty undefinied values from arrays in a type-safe way */
 export const nonNullable = <T>(value: T): value is NonNullable<T> => {
   return value !== null && value !== undefined;
 };
